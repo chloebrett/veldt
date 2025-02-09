18907a2bfad64e7fc893ca42d626f68c850008f4
Chloe Brett <chloebrett@users.noreply.github.com>
2025-02-09 22:41:18 +1100

    Create structs for storing an entire track, the notes it contains, and the synth config

--- mesic/src/main.rs
+++ mesic/src/main.rs
@@ -5,37 +5,84 @@ use std::io::Write;
 
 fn main() -> Result<(), std::io::Error> {
     let envelope = AdsrEnvelope {
-        attack: 0.5,
+        attack: 0.2,
         decay: 0.2,
-        sustain: 0.5,
+        sustain: 0.2,
         release: 0.2,
     };
-    let wave_1 = wave(0.0, 2.0, 0.2, &envelope, WaveType::Triangle);
-    let wave_2 = wave(7.0, 1.0, 0.2, &envelope, WaveType::Triangle);
+    let wave_1 = wave(0.0, 2.0, 120.0, 0.2, &envelope, WaveType::Triangle);
 
-    let envelope = AdsrEnvelope {
-        attack: 1.2,
-        decay: 0.2,
-        sustain: 0.5,
-        release: 0.2,
+    let synth = Synth {
+        wave: WaveType::Sine,
+        envelope: envelope,
+        volume: 1.,
     };
-    let wave_3 = wave(7.0, 3.0, 0.2, &envelope, WaveType::Triangle);
+    let sequence = Sequence {
+        offset: 0.,
+        volume: 1.,
+        synth: &synth,
+        notes: vec!(Note(0., 1.),
+        Note(1., 1.),
+        Note(2., 1.),
+        Note(3., 1.),
+        Note(4., 1.),
+        Note(5., 1.),
+        Note(6., 1.),
+        Note(7., 1.),
+        Note(8., 1.),
+        Note(9., 1.),
+        Note(10., 1.),
+        Note(11., 1.),
+        Note(12., 1.),
+        ),
+    };
+    let track = Track {
+        bpm: 120.,
+        sequences: vec!(sequence)
+    };
+    let output = render(&track);
 
-    let combined_wave = sum(wave_1, sum(wave_2, wave_3));
+    //let combined_wave = sum(wave_1, sum(wave_2, wave_3));
 
     let filename = "out.bin".to_string();
-    write_as_bytes(&combined_wave, filename)?;
+    write_as_bytes(&output, filename)?;
 
     Ok(())
 }
+
+#[derive(Debug)]
+struct Track<'a> {
+    bpm: Beats,
+    sequences: Vec<Sequence<'a>>
+}
+
+#[derive(Debug)]
+struct Sequence<'a> {
+    offset: Beats,
+    volume: Volume,
+    synth: &'a Synth,
+    notes: Vec<Note>
+}
+
+#[derive(Debug)]
+struct Synth {
+    wave: WaveType,
+    envelope: AdsrEnvelope,
+    volume: Volume
+}
+
+#[derive(Debug)]
+struct Note(Semitones, Beats);
 
 fn sum(a: Vec<f32>, b: Vec<f32>) -> Vec<f32> {
     a.iter().zip(b.iter()).map(|(&a,&b)| a + b).collect()
 }
 
+type Volume = f32;
 type Freq = f32;
 type Semitones = f32;
 type Seconds = f32;
+type Beats = f32;
 
 const REFERENCE_FREQUENCY: Freq = 440.0; // 440 Hz = A4
 const SAMPLE_RATE: i32 = 48_000;
@@ -51,12 +98,13 @@ fn freq(semitones: Semitones) -> Freq {
 
 #[derive(Debug)]
 struct AdsrEnvelope {
-    attack: f32,  // duration
-    decay: f32,   // duration
-    sustain: f32, // amplitude
-    release: f32, // duration
+    attack: Beats,
+    decay: Beats,
+    sustain: Volume,
+    release: Beats,
 }
 
+#[derive(Debug, Clone, Copy)]
 enum WaveType {
     Sine,
     Square,
@@ -88,7 +136,7 @@ fn triangle_wave(x: f32) -> f32 {
     }
 }
 
-fn apply_envelope(x: f32, envelope: &AdsrEnvelope, duration: Seconds) -> f32 {
+fn apply_envelope(x: f32, envelope: &AdsrEnvelope, duration: Beats) -> f32 {
     if duration < envelope.attack + envelope.decay + envelope.release {
         panic!("Envelope {:?} was too short for duration {}", envelope, duration);
     }
@@ -106,15 +154,34 @@ fn apply_envelope(x: f32, envelope: &AdsrEnvelope, duration: Seconds) -> f32 {
     }
 }
 
+fn render(track: &Track) -> Vec<f32> {
+    let bpm = track.bpm;
+    let sequences = &track.sequences;
+    // TODO: render all sequences and add them together.
+    let sequence = sequences.iter().next().unwrap();
+    let synth = &sequence.synth;
+    println!("{:?}", sequence);
+
+    let mut total_wave: Vec<f32> = vec!();
+    for note in &sequence.notes {
+        println!("{:?}", note);
+        let mut wave = wave(note.0, note.1, bpm, sequence.volume * synth.volume, &synth.envelope, synth.wave);
+        total_wave.append(&mut wave);
+    }
+
+    total_wave
+}
+
 fn wave(
     semitones: Semitones,
-    duration: Seconds,
+    beats: Beats,
+    bpm: Beats,
     volume: f32,
     envelope: &AdsrEnvelope,
     wave_type: WaveType,
 ) -> Vec<f32> {
     let step = freq(semitones) * 2.0 * PI / (SAMPLE_RATE as f32);
-    let range = 0..(SAMPLE_RATE as f32 * duration) as i32;
+    let range = 0..(SAMPLE_RATE as f32 * beats / bpm * 60.0) as i32;
 
     let wave = match wave_type {
         WaveType::Sine => |x: f32| x.sin(),
@@ -123,7 +190,7 @@ fn wave(
         WaveType::Triangle => |x: f32| triangle_wave(x),
     };
 
-    range.map(|x: i32| wave(x as f32 * step) * volume * apply_envelope(x as f32, envelope, duration)).collect()
+    range.map(|x: i32| wave(x as f32 * step) * volume * apply_envelope(x as f32, envelope, beats)).collect()
 }
 
 fn write_as_bytes(floats: &Vec<f32>, filename: String) -> Result<(), std::io::Error> {
