edfb288fab2c64112e5d3e3c035485ce6fd07180
Chloe Brett <chloebrett@users.noreply.github.com>
2025-02-09 12:57:40 +1100

    Remove hyper; just create a tonic server. Also add lockfiles.

--- xeric/src/main.rs
+++ xeric/src/main.rs
@@ -1,7 +1,5 @@
-use std::convert::Infallible;
 use std::net::SocketAddr;
 
-use hyper::service::make_service_fn;
 use shared::echo_server::{Echo, EchoServer};
 use shared::{EchoReply, EchoRequest};
 use tonic::async_trait;
@@ -21,21 +19,13 @@ impl Echo for MyEcho {
 }
 
 #[tokio::main]
-async fn main() {
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+async fn main() -> anyhow::Result<()> {
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000)).into();
 
-    let grpc_service = tonic::transport::Server::builder()
+    tonic::transport::Server::builder()
         .add_service(EchoServer::new(MyEcho))
-        .into_service();
+        .serve(addr)
+        .await?;
 
-    let make_grpc_service = make_service_fn(move |_conn| {
-        let grpc_service = grpc_service.clone();
-        async { Ok::<_, Infallible>(grpc_service) }
-    });
-
-    let server = hyper::Server::bind(&addr).serve(make_grpc_service);
-
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
+    Ok(())
 }
