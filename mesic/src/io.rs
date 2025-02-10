0e9947a710d01f486e7d00b047c17344e6a93235
Chloe Brett <chloebrett@users.noreply.github.com>
2025-02-10 16:05:10 +1100

    Add a server handler for rendering tracks

--- mesic/src/io.rs
+++ mesic/src/io.rs
@@ -2,9 +2,15 @@ use byteorder::{ByteOrder, LittleEndian};
 use std::fs;
 use std::io::Write;
 
-pub fn write_as_bytes(floats: &Vec<f32>, filename: String) -> Result<(), std::io::Error> {
+pub fn as_bytes(floats: &Vec<f32>) -> Vec<u8> {
     let mut bytes: Vec<u8> = vec![0; floats.len() * 4];
     LittleEndian::write_f32_into(&floats.as_slice(), &mut bytes);
+
+    bytes
+}
+
+pub fn write_as_bytes(floats: &Vec<f32>, filename: String) -> Result<(), std::io::Error> {
+    let bytes = as_bytes(floats);
 
     let mut file = fs::OpenOptions::new()
         .create(true)
