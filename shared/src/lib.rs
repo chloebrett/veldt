ca31c36a59c94d7802ce48e34313030543ae1eef
Chloe Brett <chloebrett@users.noreply.github.com>
2025-02-10 15:19:03 +1100

    Create serialization code; move model classes into shared lib; rename proto models to pmodel

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,1 +1,10 @@
-tonic::include_proto!("echo");
+pub mod model;
+pub mod serialize;
+
+pub mod echo {
+    tonic::include_proto!("echo");
+}
+
+pub mod pmodel {
+    tonic::include_proto!("pmodel");
+}
--- shared/src/model.rs
+++ shared/src/model.rs
@@ -1,44 +1,54 @@
-use strum::{EnumString, Display};
+use strum::{Display, EnumString};
 
 pub type Volume = f32;
 pub type Freq = f32;
 pub type Semitones = f32;
 pub type Beats = f32;
 
-#[derive(Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Track {
     pub bpm: Beats,
+
     pub synths: Vec<Synth>,
+
     pub sequences: Vec<Sequence>,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Sequence {
     pub offset: Beats,
+
     pub volume: Volume,
+
     pub synth_index: usize,
+
     pub notes: Vec<Note>,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Synth {
     pub wave: WaveType,
+
     pub envelope: AdsrEnvelope,
+
     pub volume: Volume,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Note(pub Semitones, pub Beats);
 
-#[derive(Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct AdsrEnvelope {
     pub attack: Beats,
+
     pub decay: Beats,
+
     pub sustain: Volume,
+
     pub release: Beats,
 }
 
-#[derive(Debug, Clone, Copy, EnumString, Display)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, EnumString, Display)]
 pub enum WaveType {
     Sine,
     Square,
