140621dfc2f443e98fb12ad3a8adaf2d205fafee
Chloe Brett <chloebrett@users.noreply.github.com>
2025-02-10 11:58:23 +1100

    Let the user select the wave type

--- hydric/src/app.rs
+++ hydric/src/app.rs
@@ -1,6 +1,8 @@
 use leptos::*;
 use leptos::prelude::*;
-use thaw::{Button, Space, ConfigProvider, ButtonAppearance};
+use thaw::{Card, Button, Space, ConfigProvider, ButtonAppearance, Select};
+use mesic::model::WaveType;
+use std::str::FromStr;
 
 use crate::ping::ping;
 use crate::player::AudioPlayer;
@@ -8,6 +10,11 @@ use crate::player::AudioPlayer;
 #[component]
 pub fn App() -> impl IntoView {
     let (player, set_player) = signal_local(None::<AudioPlayer>);
+    let wave_string = RwSignal::new(WaveType::Sine.to_string());
+
+    // TODO: instead of using a dependent signal, consider implementing
+    // the appropriate From trait.
+    let wave = move || WaveType::from_str(&wave_string.get()).unwrap();
 
     let ping_action = Action::new_local(|_: &()| async {
         ping().await;
@@ -15,13 +22,13 @@ pub fn App() -> impl IntoView {
 
     view! {
         <ConfigProvider>
-            <p>"Veldt"</p>
+            <h1>"Veldt"</h1>
             <Card>
                 <Space>
                     <Button
                         appearance=ButtonAppearance::Primary
                         on_click=move |_| {
-                            set_player.set(AudioPlayer::new().unwrap().into());
+                            set_player.set(AudioPlayer::new(wave()).unwrap().into());
                         }
                     >
                         "Play"
@@ -34,6 +41,12 @@ pub fn App() -> impl IntoView {
                     >
                         "Ping"
                     </Button>
+                    <Select value=wave_string>
+                    <option>Sine</option>
+                    <option>Square</option>
+                    <option>Saw</option>
+                    <option>Triangle</option>
+                    </Select>
                 </Space>
             </Card>
         </ConfigProvider>
--- hydric/src/player.rs
+++ hydric/src/player.rs
@@ -1,7 +1,7 @@
 use wasm_bindgen::prelude::*;
 use web_sys::{AudioContext};
+use mesic::model::WaveType;
 
-#[wasm_bindgen]
 pub struct AudioPlayer {
     ctx: AudioContext,
 
@@ -14,32 +14,23 @@ impl Drop for AudioPlayer {
     }
 }
 
-#[wasm_bindgen]
 impl AudioPlayer {
-    pub fn new() -> Result<AudioPlayer, JsValue> {
-        let sample_rate = 44_100; // TODO: match mesic to this
+    pub fn new(wave: WaveType) -> Result<AudioPlayer, JsValue> {
+        let sample_rate = 44_100;
         let seconds = 10;
         let channels = 1;
 
         let ctx = AudioContext::new()?;
-        let source = ctx.create_buffer_source()?;
-        let buffer = ctx.create_buffer(channels, seconds * sample_rate, sample_rate as f32)?;
 
-        // Fill the buffer with random noise
-        //let mut channel = buffer.get_channel_data(0)?;
-        let mesic_out: Vec<f32> = mesic::demo_floats();
-        //for i in 0..buffer.length() as usize {
-        //    channel[i] = 0.0;
-            // TODO:  noise!
-        //}
+        let buffer = ctx.create_buffer(channels, seconds * sample_rate, sample_rate as f32)?;
+        let mesic_out: Vec<f32> = mesic::demo_floats(wave);
         buffer.copy_to_channel(&mesic_out.as_slice(), 0)?;
 
-        // Connect the buffer to the AudioContext destination (aka
-        // the user's speakers).
+        let source = ctx.create_buffer_source()?;
         source.set_buffer(Some(&buffer));
         source.connect_with_audio_node(&ctx.destination())?;
 
-        // Start playing the buffer!
+        // Start playing!
         source.start()?;
 
         Ok(AudioPlayer {
