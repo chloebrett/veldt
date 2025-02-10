61629583a726c9f2026798b22a0de3aac883a77b
Chloe Brett <chloebrett@users.noreply.github.com>
2025-02-10 23:33:05 +1100

    Get egui minimal working

--- experimental/egui-minimal/src/main.rs
+++ experimental/egui-minimal/src/main.rs
@@ -50,7 +50,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(egui_minimal::TemplateApp::new(cc)))),
             )
             .await;
 
