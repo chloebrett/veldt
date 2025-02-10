39d1cb83fb7c4b9df341b5c0f57447f22584e5aa
Chloe Brett <chloebrett@users.noreply.github.com>
2025-02-10 11:35:37 +1100

    Start creating a thaw UI

--- hydric/src/app.rs
+++ hydric/src/app.rs
@@ -1,5 +1,6 @@
 use leptos::*;
 use leptos::prelude::*;
+use thaw::{Button, Space, ConfigProvider, ButtonAppearance};
 
 use crate::ping::ping;
 use crate::player::AudioPlayer;
@@ -8,20 +9,34 @@ use crate::player::AudioPlayer;
 pub fn App() -> impl IntoView {
     let (player, set_player) = signal_local(None::<AudioPlayer>);
 
-    let ping_action = Action::new_local(|input: &()| async {
+    let ping_action = Action::new_local(|_: &()| async {
         ping().await;
     });
 
     view! {
-        <div>
-            <p>"Hello, world!"</p>
-            <button on:click=move |_| {
-                ping_action.dispatch(());
-            }>Ping</button>
-            <button on:click=move |_| {
-                set_player.set(AudioPlayer::new().unwrap().into());
-            }>Play</button>
-        </div>
+        <ConfigProvider>
+            <p>"Veldt"</p>
+            <Card>
+                <Space>
+                    <Button
+                        appearance=ButtonAppearance::Primary
+                        on_click=move |_| {
+                            set_player.set(AudioPlayer::new().unwrap().into());
+                        }
+                    >
+                        "Play"
+                    </Button>
+                    <Button
+                        appearance=ButtonAppearance::Secondary
+                        on_click=move |_| {
+                            ping_action.dispatch(());
+                        }
+                    >
+                        "Ping"
+                    </Button>
+                </Space>
+            </Card>
+        </ConfigProvider>
     }
 }
 
