29541b90565a27bfbb74371679f97d87d5d4d91d
elise <elise.dev.wheeler@gmail.com>
2025-02-14 22:09:07 +1100

    Replace piecewise functions with waveform functions

--- mesic/src/wave.rs
+++ mesic/src/wave.rs
@@ -3,7 +3,6 @@ use std::f32::consts::PI;
 use crate::WaveType;
 
 const TAU: f32 = 2.0 * PI;
-const HALF_PI: f32 = 0.5 * PI;
 
 pub fn make_wave(x: f32, wave_type: WaveType) -> f32 {
     let x = x % TAU;
@@ -17,19 +16,13 @@ pub fn make_wave(x: f32, wave_type: WaveType) -> f32 {
 }
 
 fn square_wave(x: f32) -> f32 {
-    if x > PI { -1.0 } else { 1.0 }
+    x.sin().signum()
 }
 
 fn saw_wave(x: f32) -> f32 {
-    if x > PI { x / PI - 2.0 } else { x / PI }
+    (x/2.0).tan().atan() * 2.0 / PI
 }
 
 fn triangle_wave(x: f32) -> f32 {
-    if x < HALF_PI {
-        x / HALF_PI
-    } else if x < 3.0 * HALF_PI {
-        2.0 * (1.0 - x / PI)
-    } else {
-        2.0 * (x / PI) - 4.0
-    }
+    x.sin().asin() * 2.0 / PI
 }
