5243295e0edf33d9c9e7d40d1601f26ed2042cb6
Elise <elisedemarie@users.noreply.github.com>
2025-02-15 11:08:19 +1100

    Change division for multiplication of inverses

--- mesic/src/wave.rs
+++ mesic/src/wave.rs
@@ -3,7 +3,8 @@ use std::f32::consts::PI;
 use crate::WaveType;
 
 const TAU: f32 = 2.0 * PI;
-const HALF_PI: f32 = PI / 2.0;
+const HALF_PI: f32 = 0.5* PI;
+const INV_HALF_PI: f32 = HALF_PI.recip();
 
 pub fn make_wave(x: f32, wave_type: WaveType) -> f32 {
     let x = x % TAU;
@@ -21,9 +22,9 @@ fn square_wave(x: f32) -> f32 {
 }
 
 fn saw_wave(x: f32) -> f32 {
-    (x/2.0).tan().atan() / HALF_PI 
+    (x*0.5).tan().atan() * INV_HALF_PI 
 }
 
 fn triangle_wave(x: f32) -> f32 {
-    x.sin().asin() / HALF_PI 
+    x.sin().asin() * INV_HALF_PI 
 }
