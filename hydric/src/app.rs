f4a21637e66becd94980f3a9690dbcb666ff6dff
Chloe Brett <chloebrett@users.noreply.github.com>
2025-03-01 17:00:04 +1100

    Support note durations in note selection list

--- hydric/src/app.rs
+++ hydric/src/app.rs
@@ -7,12 +7,11 @@ use thaw::{
     Accordion, AccordionHeader, AccordionItem, Button, ButtonAppearance, Card, ConfigProvider,
     Select, Slider, Space, SpinButton,
 };
-
 use crate::ping::ping;
 use crate::player::AudioPlayer;
 use crate::render::render;
-use shared::model::track::Track;
 use mesic::{create_demo_track, create_track, render as local_render};
+use shared::model::note::Note;
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -27,6 +26,9 @@ pub fn App() -> impl IntoView {
     let note1 = RwSignal::new(0);
     let note2 = RwSignal::new(1);
     let note3 = RwSignal::new(2);
+    let duration1 = RwSignal::new(1.0);
+    let duration2 = RwSignal::new(1.0);
+    let duration3 = RwSignal::new(2.0);
 
     // TODO: instead of using a dependent signal, consider implementing
     // the appropriate From trait.
@@ -34,19 +36,34 @@ pub fn App() -> impl IntoView {
     let demo_option = move || DemoOption::from_str(&demo_string.get()).unwrap();
     let bpm = move || bpm_value.get();
     let volume = move || (volume_percent.get() / 100.0f64) as f32;
-    let notes = move || vec!(note1.get(), note2.get(), note3.get());
+    let notes = move || {
+        vec![
+            Note(note1.get() as f32, duration1.get()),
+            Note(note2.get() as f32, duration2.get()),
+            Note(note3.get() as f32, duration3.get()),
+        ]
+    };
 
     let ping_action = Action::new_local(|_: &()| async {
         ping().await;
     });
     let track = move || match demo_option() {
-        DemoOption::Custom => create_track(notes(), wave(), bpm(), volume(), transpose_semitones.get()),
-        _ => create_demo_track(demo_option(), wave(), bpm(), volume(), transpose_semitones.get()),
+        DemoOption::Custom => {
+            create_track(notes(), wave(), bpm(), volume(), transpose_semitones.get())
+        }
+        _ => create_demo_track(
+            demo_option(),
+            wave(),
+            bpm(),
+            volume(),
+            transpose_semitones.get(),
+        ),
     };
 
     // Continually re-request audio from the server then the wave type changes.
-    let server_audio =
-        LocalResource::new(move || render(demo_option(), notes(), wave(), bpm(), volume(), transpose_semitones.get()));
+    let server_audio = LocalResource::new(move || {
+        render(track())
+    });
 
     view! {
         <ConfigProvider>
@@ -132,9 +149,18 @@ pub fn App() -> impl IntoView {
                     <SpinButton<i32> value=transpose_semitones step_page=1 min=-24 max=24 />
             </Card>
             <Card>
+            <Space>
                     <SpinButton<i32> value=note1 step_page=1 min=-24 max=24 />
+                    <SpinButton<f32> value=duration1 step_page=0.25 min=0.5 max=16.0 />
+                    </Space>
+            <Space>
                     <SpinButton<i32> value=note2 step_page=1 min=-24 max=24 />
+                    <SpinButton<f32> value=duration2 step_page=0.25 min=0.5 max=16.0 />
+                    </Space>
+                    <Space>
                     <SpinButton<i32> value=note3 step_page=1 min=-24 max=24 />
+                    <SpinButton<f32> value=duration3 step_page=0.25 min=0.5 max=16.0 />
+                    </Space>
             </Card>
         </ConfigProvider>
     }
