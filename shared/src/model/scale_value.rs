c9610ebf509e9c1431ad96b97d794616ecbec1fa
Elise <elisedemarie@users.noreply.github.com>
2025-03-02 13:05:01 +1100

    Add ScaleValue type

---
+++ shared/src/model/scale_value.rs
@@ -0,0 +1,16 @@
+#[derive(Clone, Debug, PartialEq)]
+pub enum ScaleValue {
+    A,
+    ASharp,
+    B,
+    C,
+    CSharp,
+    D,
+    DSharp,
+    E,
+    F,
+    FSharp,
+    G,
+    GSharp,
+}
+
--- shared/src/types.rs
+++ shared/src/types.rs
@@ -2,3 +2,6 @@ pub type Volume = f32;
 pub type Freq = f32;
 pub type Semitones = f32;
 pub type Beats = f32;
+pub type Octave = u8;
+pub type PitchValue = u32
+
