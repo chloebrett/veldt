f0a70445972051da5d74bd515968344ba2a5b00b
elise <elise.dev.wheeler@gmail.com>
2025-03-02 13:42:52 +1100

    Create proto for PitchName

--- shared/src/model/pitch_name.rs
+++ shared/src/model/pitch_name.rs
@@ -1,3 +1,4 @@
+use crate::pmodel::*;
 use crate::model::scale_value::ScaleValue;
 use crate::types::*;
 
@@ -5,4 +6,24 @@ use crate::types::*;
 pub struct PitchName {
     pub scale_value: ScaleValue,
     pub octave: Octave
+}
+
+impl From<PitchNameProto> for PitchName {
+    fn from(item: PitchNameProto) -> Self {
+        PitchName {
+            scale_value: TryInto::<ScaleValueProto>::try_into(item.scale_value)
+                .unwrap()
+                .into(),
+            octave: item.octave
+        }
+    }
+}
+
+impl From<PitchName> for PitchNameProto {
+    fn from(item: PitchName) -> Self {
+        PitchNameProto {
+            scale_value: Into::<ScaleValueProto>::into(item.scale_value) as i32,
+            octave: item.octave,
+        }
+    }
 }
