369a7d1bbaf505a3685f8b49bc048dd4d4315736
Elise <elisedemarie@users.noreply.github.com>
2025-03-05 21:10:26 +1100

    Add more scales.

--- mesic/src/scale.rs
+++ mesic/src/scale.rs
@@ -6,7 +6,9 @@ pub fn create_scale_values(scale: Scale, key: ScaleValue) -> Vec<ScaleValue> {
     let pitch_values = match scale {
         Scale::Chromatic => vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
         Scale::Major => vec![0, 2, 4, 5, 7, 9, 11],
-        Scale::HarmonicMinor => vec![0, 2, 3, 5, 7, 8, 10]
+        Scale::NaturalMinor => vec![0, 2, 3, 5, 7, 8, 10],
+        Scale::HarmonicMinor => vec![0, 2, 3, 5, 7, 8, 11],
+        Scale::Pentatonic => vec![0, 2, 4, 7, 9]
     }; 
 
     let key_value = <ScaleValue as Into<PitchValue>>::into(key);
