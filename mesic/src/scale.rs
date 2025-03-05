f0523e21de2aa724aa77514873401751a9309da9
Elise <elisedemarie@users.noreply.github.com>
2025-03-05 21:02:43 +1100

    Render note options based on key and scale.

--- mesic/src/scale.rs
+++ mesic/src/scale.rs
@@ -4,8 +4,8 @@ use crate::ScaleValue;
 
 pub fn create_scale_values(scale: Scale, key: ScaleValue) -> Vec<ScaleValue> {
     let pitch_values = match scale {
-        Scale::Chromatic => vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        Scale::Major => vec![0, 2, 3, 5, 6, 8, 10],
+        Scale::Chromatic => vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+        Scale::Major => vec![0, 2, 4, 5, 7, 9, 11],
         Scale::HarmonicMinor => vec![0, 2, 3, 5, 7, 8, 10]
     }; 
 
