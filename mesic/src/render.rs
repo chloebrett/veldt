75c0fee9ecf0e1527f192056ab766373291261f4
Chloe Brett <chloebrett@users.noreply.github.com>
2025-03-09 14:40:55 +1100

    Allow the user to configure the delay effect; pull effect config up into the UI.

--- mesic/src/render.rs
+++ mesic/src/render.rs
@@ -2,46 +2,17 @@ use crate::effect::apply_effects;
 use crate::sig::sum;
 use crate::wave::{SupersawConfig, polyphonic_wave};
 use shared::model::{
-    DelayConfig, Effect, EffectInstance, EffectMeta, EqConfig, EqType, MixerChannel, Track,
+    EffectInstance, MixerChannel, Track,
 };
-use shared::types::{Freq, KnobPosition};
 
 pub fn render(
     track: &Track,
     supersaw_config: SupersawConfig,
-    resonant_freq: Freq,
-    resonance_q: KnobPosition,
-    resonance_wet: KnobPosition,
+    effects: Vec<EffectInstance>,
 ) -> Vec<f32> {
     let bpm = track.bpm;
     let mut total_wave: Vec<f32> = vec![];
 
-    let delay = EffectInstance {
-        effect: Effect::SimpleDelay {
-            config: DelayConfig {
-                amplitude: 0.5,
-
-                delay_ms: 250.0,
-            },
-        },
-        meta: EffectMeta { id: 0, wet: 0.5 },
-    };
-    let eq = EffectInstance {
-        effect: Effect::SimpleEq {
-            config: EqConfig {
-                kind: EqType::SimpleSecondOrderBandStop,
-
-                fc: resonant_freq,
-
-                q: resonance_q,
-            },
-        },
-        meta: EffectMeta {
-            id: 1,
-            wet: resonance_wet,
-        },
-    };
-    let effects = vec![delay, eq];
     let mixer_channel = MixerChannel { effects };
 
     for sequence in &track.sequences {
