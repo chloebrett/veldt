26208c64e85045831f55ea2f3e602467c1b56c32
elise <elise.dev.wheeler@gmail.com>
2025-03-10 22:00:26 +1100

    Remove debug logging.

--- hydric/src/note_save.rs
+++ hydric/src/note_save.rs
@@ -4,7 +4,6 @@ use shared::save_notes::{
     save_notes_client::SaveNotesClient,
 };
 use tonic_web_wasm_client::Client;
-use web_sys::console;
 
 pub async fn save_notes(name: String, notes: Vec<Note>) {
     let base_url = "http://127.0.0.1:3000".to_string();
@@ -25,7 +24,5 @@ pub async fn load_note_list() -> Vec<String> {
     let mut grpc = LoadNotesListClient::new(wasm_client);
 
     let result = grpc.load_notes_list(LoadNotesListRequest {}).await;
-    let names = result.unwrap().into_inner().names;
-    console::log_1(&format!("{:?}", names).into());
-    names
+    result.unwrap().into_inner().names
 }
