c280133879e89509e8e51cfb91041e5f698e75d9
Elise <elisedemarie@users.noreply.github.com>
2025-03-11 17:20:27 +1100

    Add load and play from server buttons.

--- hydric/src/app.rs
+++ hydric/src/app.rs
@@ -1,4 +1,5 @@
 use crate::audio_player::{Handle, play};
+use crate::audio_render::render;
 use crate::note_save::{load_note_list, load_notes, save_notes};
 use egui::{
     Color32, Rect, ScrollArea, Ui, containers::Frame, emath, epaint, epaint::PathStroke, pos2,
@@ -336,63 +337,19 @@ impl App {
         if let Some(render_promise) = &self.server_render_promise {
            if let Some(server_audio) = render_promise.ready() {
                if ui.button("Play (server)").clicked() {
-                   let audio = server_audio.to_vec().into_iter()
+                   self.audio = server_audio.to_vec().into_iter()
                        .map(|sample| sample.clamp(-1.0, 1.0))
                        .collect::<Vec<f32>>();
-                    self.handle = Some(play(&audio))
+                    self.handle = Some(play(&self.audio))
                }
            } 
-        } else {
-            if ui.button("Load audio (server)").clicked() {
+        }
+        if ui.button("Load audio (server)").clicked() {
 
-                let envelope = AdsrEnvelope {
-                    attack: self.attack,
-                    decay: self.decay,
-                    sustain: self.sustain,
-                    release: self.release,
-                };
-                let track = create_track(self.notes.clone());
-                let delay = EffectInstance {
-                    effect: Effect::SimpleDelay {
-                        config: DelayConfig {
-                            amplitude: self.delay_amplitude,
-
-                            delay_ms: self.delay_ms,
-                        },
-                    },
-                    meta: EffectMeta {
-                        id: 0,
-                        wet: self.delay_wet,
-                    },
-                };
-                let eq = EffectInstance {
-                    effect: Effect::SimpleEq {
-                        config: EqConfig {
-                            kind: self.eq_type.clone(),
-                            fc: self.resonant_freq,
-                            q: self.q,
-                        },
-                    },
-                    meta: EffectMeta {
-                        id: 1,
-                        wet: self.eq_wet,
-                    },
-                };
-                let effects = vec![delay, eq];
-                let generator = GeneratorInstance {
-                    id: 0,
-
-                    kind: GeneratorType::SimpleWave {
-                        config: SimpleWaveConfig {
-                            wave: self.wave_type,
-
-                            envelope,
-                        },
-                    },
-
-                    meta: GeneratorMeta { volume: 1.0 },
-                };
-            }
+            let track = create_track(self.notes.clone());
+            self.server_render_promise = Some(Promise::spawn_local(
+                    async move {render(track).await}
+                    ))
         }
         self.audio_vis(ui);
     }
