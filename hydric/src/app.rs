7b5c8d7f74336fe7e27247a7dee040408295f44b
Elise <elisedemarie@users.noreply.github.com>
2025-03-11 17:21:02 +1100

    Format

--- hydric/src/app.rs
+++ hydric/src/app.rs
@@ -42,7 +42,7 @@ pub struct App {
     handle: Option<Handle>,
     notes_list_promise: Promise<Vec<String>>,
     notes_promise: Option<Promise<Vec<Note>>>,
-    server_render_promise: Option<Promise<Vec<f32>>>
+    server_render_promise: Option<Promise<Vec<f32>>>,
 }
 
 impl Default for App {
@@ -79,7 +79,7 @@ impl Default for App {
             handle: None,
             notes_list_promise: Promise::spawn_local(async move { load_note_list().await }),
             notes_promise: None,
-            server_render_promise: None
+            server_render_promise: None,
         }
     }
 }
@@ -335,21 +335,21 @@ impl App {
             self.handle = Some(play(&self.audio));
         }
         if let Some(render_promise) = &self.server_render_promise {
-           if let Some(server_audio) = render_promise.ready() {
-               if ui.button("Play (server)").clicked() {
-                   self.audio = server_audio.to_vec().into_iter()
-                       .map(|sample| sample.clamp(-1.0, 1.0))
-                       .collect::<Vec<f32>>();
+            if let Some(server_audio) = render_promise.ready() {
+                if ui.button("Play (server)").clicked() {
+                    self.audio = server_audio
+                        .to_vec()
+                        .into_iter()
+                        .map(|sample| sample.clamp(-1.0, 1.0))
+                        .collect::<Vec<f32>>();
                     self.handle = Some(play(&self.audio))
-               }
-           } 
+                }
+            }
         }
         if ui.button("Load audio (server)").clicked() {
-
             let track = create_track(self.notes.clone());
-            self.server_render_promise = Some(Promise::spawn_local(
-                    async move {render(track).await}
-                    ))
+            self.server_render_promise =
+                Some(Promise::spawn_local(async move { render(track).await }))
         }
         self.audio_vis(ui);
     }
