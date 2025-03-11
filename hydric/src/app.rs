795b472403e9695ad3650f1a3089f7de4c3a141b
Elise <elisedemarie@users.noreply.github.com>
2025-03-11 20:50:15 +1100

    Wrap save server methods in Options.

--- hydric/src/app.rs
+++ hydric/src/app.rs
@@ -40,9 +40,10 @@ pub struct App {
     delay_wet: f32,
     delay_amplitude: f32,
     handle: Option<Handle>,
-    notes_list_promise: Promise<Vec<String>>,
-    notes_promise: Option<Promise<Vec<Note>>>,
+    notes_list_promise: Promise<Option<Vec<String>>>,
+    notes_promise: Option<Promise<Option<Vec<Note>>>>,
     server_render_promise: Option<Promise<Vec<f32>>>,
+    save_notes_promise: Option<Promise<Option<()>>>,
 }
 
 impl Default for App {
@@ -80,6 +81,7 @@ impl Default for App {
             notes_list_promise: Promise::spawn_local(async move { load_note_list().await }),
             notes_promise: None,
             server_render_promise: None,
+            save_notes_promise: None,
         }
     }
 }
@@ -243,12 +245,15 @@ impl App {
             let save_name = self.track_name.clone();
             let notes_to_save = self.notes.clone();
             let _save_thread =
-                Promise::spawn_local(async move { save_notes(save_name, notes_to_save).await });
+                self.save_notes_promise = Some(Promise::spawn_local(async move { save_notes(save_name, notes_to_save).await }));
             self.notes_list_promise = Promise::spawn_local(async move { load_note_list().await });
         }
 
         if let Some(list) = self.notes_list_promise.ready() {
-            self.track_list = list.to_vec()
+            match list {
+                Some(values) => self.track_list = values.to_vec(),
+                None => self.track_list = vec![]
+            }
         }
         egui::ComboBox::from_label("Saved Tracks")
             .selected_text(self.track_name.clone())
@@ -265,7 +270,10 @@ impl App {
         }
         if let Some(notes_promise) = &self.notes_promise {
             if let Some(notes) = notes_promise.ready() {
-                self.notes = notes.to_vec()
+                match notes {
+                    Some(values) => self.notes = values.to_vec(),
+                    None => {}
+                }
             }
         }
     }
