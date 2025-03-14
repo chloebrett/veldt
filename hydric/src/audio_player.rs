010d97487410be801cf7513b73b113112f88fc4a
Chloe Brett <chloebrett@users.noreply.github.com>
2025-03-14 22:25:15 +1100

    Clean up the audio player code.

--- hydric/src/audio_player.rs
+++ hydric/src/audio_player.rs
@@ -1,11 +1,10 @@
 use chrono::{DateTime, Utc};
 use cpal::Stream;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use cpal::{FromSample, SizedSample};
 use web_sys::console;
 
 pub struct Handle {
-    stream: Stream,
+    _stream: Stream,
     pub start_timestamp: DateTime<Utc>,
 }
 
@@ -15,32 +14,15 @@ pub fn play(audio: &[f32]) -> Handle {
         .default_output_device()
         .expect("failed to find a default output device");
     let config = device.default_output_config().unwrap();
+    let config: &cpal::StreamConfig = &config.into();
 
-    match config.sample_format() {
-        cpal::SampleFormat::F32 => run::<f32>(&device, &config.into(), audio),
-        cpal::SampleFormat::I16 => run::<i16>(&device, &config.into(), audio),
-        cpal::SampleFormat::U16 => run::<u16>(&device, &config.into(), audio),
-        // not all supported sample formats are included in this example
-        _ => panic!("Unsupported sample format!"),
-    }
-}
-
-fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig, audio: &[f32]) -> Handle
-where
-    T: SizedSample + FromSample<f32>,
-{
     let channels = config.channels as usize;
 
     let mut sample_clock: usize = 0;
     let audio = audio.to_owned();
-    let mut next_value = move || {
-        if sample_clock >= audio.len() {
-            0.0
-        } else {
-            let sample = audio[sample_clock];
-            sample_clock += 1;
-            sample
-        }
+    let mut next_sample = move || {
+        sample_clock += 1;
+        *audio.get(sample_clock).unwrap_or(&0.0)
     };
 
     // TODO: replace with egui logger
@@ -49,26 +31,20 @@ where
     let stream = device
         .build_output_stream(
             config,
-            move |data: &mut [T], _| write_data(data, channels, &mut next_value),
+            move |data: &mut [f32], _| {
+                for frame in data.chunks_mut(channels) {
+                    for sample in frame.iter_mut() {
+                        *sample = next_sample();
+                    }
+                }
+            },
             err_fn,
             None,
         )
         .unwrap();
     stream.play().unwrap();
     Handle {
-        stream,
+        _stream: stream,
         start_timestamp: chrono::offset::Utc::now(),
-    }
-}
-
-fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
-where
-    T: SizedSample + FromSample<f32>,
-{
-    for frame in output.chunks_mut(channels) {
-        let value: T = T::from_sample(next_sample());
-        for sample in frame.iter_mut() {
-            *sample = value;
-        }
     }
 }
