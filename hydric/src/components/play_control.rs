f0db9cc05e8350aa9263bc5c5c6fd4e212df24dc
Chloe Brett <chloebrett@users.noreply.github.com>
2025-03-14 22:13:58 +1100

    Use references instead of cloning in more places.

--- hydric/src/components/play_control.rs
+++ hydric/src/components/play_control.rs
@@ -9,7 +9,7 @@ use poll_promise::Promise;
 pub fn play_control(app: &mut App, ui: &mut Ui) {
     if ui.button("Play (local)").clicked() {
         let track = create_track(app.notes.clone());
-        app.audio = local_render(&track, app.effects.clone(), app.generator.clone(), app.bpm)
+        app.audio = local_render(&track, &app.mixer_channels[0], &app.generator, app.bpm)
             .into_iter()
             .map(|sample| sample.clamp(-1.0, 1.0))
             .collect();
@@ -31,10 +31,10 @@ pub fn play_control(app: &mut App, ui: &mut Ui) {
     if ui.button("Load audio (server)").clicked() {
         let track = create_track(app.notes.clone());
         let generator = app.generator.clone();
-        let effects = app.effects.clone();
+        let mixer_channel = app.mixer_channels[0].effects.clone();
         let bpm = app.bpm;
         app.server_render_promise = Some(Promise::spawn_local(async move {
-            render(track, effects, generator, bpm).await
+            render(track, mixer_channel, generator, bpm).await
         }))
     }
     audio_vis(app, ui);
