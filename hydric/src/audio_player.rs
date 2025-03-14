c85d9e53ac89dcfa84bfa1da8261ef4d111a803f
Chloe Brett <chloebrett@users.noreply.github.com>
2025-03-14 22:44:50 +1100

    Define traits for sample

--- hydric/src/audio_player.rs
+++ hydric/src/audio_player.rs
@@ -16,25 +16,24 @@ pub fn play(audio: &[f32]) -> Handle {
     let config = device.default_output_config().unwrap();
     let config: &cpal::StreamConfig = &config.into();
 
-    let channels = config.channels as usize;
-
     let mut sample_clock: usize = 0;
     let audio = audio.to_owned();
     let mut next_sample = move || {
         sample_clock += 1;
         *audio.get(sample_clock).unwrap_or(&0.0)
     };
-
     // TODO: replace with egui logger
     let err_fn = |err| console::error_1(&format!("an error occurred on stream: {}", err).into());
+    let channels = config.channels as usize;
 
     let stream = device
         .build_output_stream(
             config,
             move |data: &mut [f32], _| {
                 for frame in data.chunks_mut(channels) {
+                    let value = next_sample();
                     for sample in frame.iter_mut() {
-                        *sample = next_sample();
+                        *sample = value;
                     }
                 }
             },
