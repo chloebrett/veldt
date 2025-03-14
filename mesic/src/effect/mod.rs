a7aa6720ab514158672dcb6d834389389b1463d8
Chloe Brett <chloebrett@users.noreply.github.com>
2025-03-14 22:13:58 +1100

    Use references instead of cloning in more places.

--- mesic/src/effect/mod.rs
+++ mesic/src/effect/mod.rs
@@ -11,8 +11,8 @@ pub trait ApplyEffect {
     fn apply(&self, input: &[f32]) -> Vec<f32>;
 }
 
-pub fn apply_effects(signal: Vec<f32>, effects: Vec<EffectInstance>) -> Vec<f32> {
-    let mut output = signal.clone();
+pub fn apply_effects(signal: &[f32], effects: &[EffectInstance]) -> Vec<f32> {
+    let mut output = signal.to_owned();
 
     for effect in effects {
         output = apply_effect(output, effect);
@@ -22,16 +22,16 @@ pub fn apply_effects(signal: Vec<f32>, effects: Vec<EffectInstance>) -> Vec<f32>
 }
 
 /// Mixes two signals in the given dry/wet ratio.
-fn mix(dry: Vec<f32>, wet: Vec<f32>, ratio: KnobPosition) -> Vec<f32> {
-    sum(mult(wet, ratio), mult(dry, 1.0 - ratio))
+fn mix(dry: &Vec<f32>, wet: &Vec<f32>, ratio: KnobPosition) -> Vec<f32> {
+    sum(&mult(wet, ratio), &mult(dry, 1.0 - ratio))
 }
 
-fn apply_effect(dry_signal: Vec<f32>, effect: EffectInstance) -> Vec<f32> {
-    let wet_signal = match effect.effect {
+fn apply_effect(dry_signal: Vec<f32>, effect: &EffectInstance) -> Vec<f32> {
+    let wet_signal = match &effect.effect {
         Effect::SimpleDelay { config } => config.apply(&dry_signal),
         Effect::SimpleEq { config } => config.apply(&dry_signal),
         _ => panic!("Effect not implemented yet!"),
     };
 
-    mix(dry_signal, wet_signal, effect.meta.wet)
+    mix(&dry_signal, &wet_signal, effect.meta.wet)
 }
