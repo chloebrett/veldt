6ee188d7ba289ab50bedbb31e3925e43ad06d399
Chloe Brett <chloebrett@users.noreply.github.com>
2025-03-14 21:47:47 +1100

    Store effects in a list in the UI.

--- hydric/src/components/app.rs
+++ hydric/src/components/app.rs
@@ -21,8 +21,7 @@ pub struct App {
     pub volume: f32,
     pub bpm: f32,
     pub generator: GeneratorInstance,
-    pub eq: EffectInstance,
-    pub delay: EffectInstance,
+    pub effects: Vec<EffectInstance>,
     pub audio: Vec<f32>,
     pub notes: Vec<Note>,
     pub key: ScaleValue,
@@ -58,25 +57,27 @@ impl Default for App {
                 },
                 meta: GeneratorMeta { volume: 1.0 },
             },
-            eq: EffectInstance {
-                effect: Effect::SimpleEq {
-                    config: EqConfig {
-                        kind: EqType::SimpleResonator,
-                        fc: 1000.0,
-                        q: 1.0,
+            effects: vec![
+                EffectInstance {
+                    effect: Effect::SimpleEq {
+                        config: EqConfig {
+                            kind: EqType::SimpleResonator,
+                            fc: 1000.0,
+                            q: 1.0,
+                        },
                     },
+                    meta: EffectMeta { id: 0, wet: 1.0 },
                 },
-                meta: EffectMeta { id: 0, wet: 1.0 },
-            },
-            delay: EffectInstance {
-                effect: Effect::SimpleDelay {
-                    config: DelayConfig {
-                        amplitude: 0.5,
-                        delay_ms: 250.0,
+                EffectInstance {
+                    effect: Effect::SimpleDelay {
+                        config: DelayConfig {
+                            amplitude: 0.5,
+                            delay_ms: 250.0,
+                        },
                     },
+                    meta: EffectMeta { id: 1, wet: 0.5 },
                 },
-                meta: EffectMeta { id: 1, wet: 0.5 },
-            },
+            ],
             audio: vec![],
             notes: vec![Note {
                 pitch_name: PitchName {
@@ -136,10 +137,10 @@ impl eframe::App for App {
                     envelope_control(&mut generator_config.envelope, ui);
                     ui.separator();
                     generator_control(generator_config, ui);
-                    ui.separator();
-                    effect_control(&mut self.eq, ui);
-                    ui.separator();
-                    effect_control(&mut self.delay, ui);
+                    for i in 0..self.effects.len() {
+                        ui.separator();
+                        effect_control(&mut self.effects[i], ui);
+                    }
                     ui.separator();
                     key_control(self, ui);
                     ui.separator();
