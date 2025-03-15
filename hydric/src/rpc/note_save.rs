cd674421096e8a7b20d744cb63361db65b28ad6d
elise <elise.dev.wheeler@gmail.com>
2025-03-15 20:44:36 +1100

    Implement load_track in client.

--- hydric/src/rpc/note_save.rs
+++ hydric/src/rpc/note_save.rs
@@ -3,9 +3,10 @@ use shared::model::{Note, Track};
 use shared::save_notes::{
     LoadNotesRequest, load_notes_client::LoadNotesClient,
 };
+use shared::save_track::load_track_client::LoadTrackClient;
 use shared::save_track::load_track_list_client::LoadTrackListClient;
 use shared::save_track::save_track_client::SaveTrackClient;
-use shared::save_track::{LoadTrackListRequest, SaveTrackRequest};
+use shared::save_track::{LoadTrackListRequest, LoadTrackRequest, SaveTrackRequest};
 use shared::serialize::map_vec;
 use tonic_web_wasm_client::Client;
 use web_sys::console;
@@ -41,6 +42,21 @@ pub async fn load_track_list() -> Option<Vec<String>> {
             None
         }
     }
+}
+
+pub async fn load_track(name: String) -> Option<Track> {
+    let client = Client::new(XERIC_URL.to_string());
+    let mut grpc = LoadTrackClient::new(client);
+
+    let result = grpc.load_track(LoadTrackRequest { name }).await;
+
+    if let Ok(load_track_reply) = result {
+        if let Some(track) = load_track_reply.into_inner().track {
+            return Some(track.into())
+        }
+    }
+    console::error_1(&"Error loading track from server.".into());
+    None
 }
 
 pub async fn load_notes(name: String) -> Option<Vec<Note>> {
