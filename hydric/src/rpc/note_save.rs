c1ac0fa4f7afe4466e67192a58461be35bb5b87f
Elise <elisedemarie@users.noreply.github.com>
2025-03-15 20:06:57 +1100

    Implement load track list on client.

--- hydric/src/rpc/note_save.rs
+++ hydric/src/rpc/note_save.rs
@@ -4,8 +4,9 @@ use shared::save_notes::{
     LoadNotesListRequest, LoadNotesRequest, load_notes_client::LoadNotesClient,
     load_notes_list_client::LoadNotesListClient,
 };
+use shared::save_track::load_track_list_client::LoadTrackListClient;
 use shared::save_track::save_track_client::SaveTrackClient;
-use shared::save_track::SaveTrackRequest;
+use shared::save_track::{LoadTrackListRequest, SaveTrackRequest};
 use shared::serialize::map_vec;
 use tonic_web_wasm_client::Client;
 use web_sys::console;
@@ -24,6 +25,20 @@ pub async fn save_track(name: String, track: Track) -> Option<()> {
         Ok(_) => Some(()),
         Err(_) => {
             console::error_1(&"Error saving notes to server.".into());
+            None
+        }
+    }
+}
+
+pub async fn load_track_list() -> Option<Vec<String>> {
+    let client = Client::new(XERIC_URL.to_string());
+    let mut grpc = LoadTrackListClient::new(client);
+
+    let result = grpc.load_track_list(LoadTrackListRequest {}).await;
+    match result {
+        Ok(response) => Some(response.into_inner().names),
+        Err(_) => {
+            console::error_1(&"Error loading track list from server.".into());
             None
         }
     }
