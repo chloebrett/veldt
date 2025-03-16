8f3ebdcfae96282e7f9a6234670d16156191610c
Elise <elisedemarie@users.noreply.github.com>
2025-03-16 13:51:37 +1100

    Add note value to PlacedNote equality comparison.

--- shared/src/model/track.rs
+++ shared/src/model/track.rs
@@ -68,7 +68,8 @@ impl Ord for PlacedNote {
 // TODO: consider including note identity in this definition.
 impl PartialEq for PlacedNote {
     fn eq(&self, other: &Self) -> bool {
-        self.offset == other.offset
+        (self.offset == other.offset)
+            & (self.note == other.note)
     }
 }
 
