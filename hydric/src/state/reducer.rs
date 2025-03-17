f6f4d80481a01af30b3ac43ecdcb5b0386893780
Chloe Brett <chloebrett@users.noreply.github.com>
2025-03-17 16:56:25 +1100

    Use action dispatch for envelope controls.

--- hydric/src/state/reducer.rs
+++ hydric/src/state/reducer.rs
@@ -81,5 +81,32 @@ pub fn reducer(store: &mut Store, action: Action) {
             };
             generator_config.detune_cents = detune_cents;
         }
+        Action::SetEnvelope {
+            generator_index,
+            envelope,
+        } => {
+            let mut envelope = envelope.clone();
+            let headroom = 1.0 - envelope.attack - envelope.decay - envelope.release;
+            let max_attack = headroom + envelope.attack;
+            let max_decay = headroom + envelope.decay;
+            let max_release = headroom + envelope.release;
+
+            if envelope.attack > max_attack {
+                envelope.attack = max_attack;
+            }
+            if envelope.decay > max_decay {
+                envelope.decay = max_decay;
+            }
+            if envelope.release > max_release {
+                envelope.release = max_release;
+            }
+
+            let generator_type: &mut GeneratorType =
+                &mut store.project.generators[generator_index].kind;
+            let generator_config: &mut SimpleWaveConfig = match generator_type {
+                GeneratorType::SimpleWave { config } => config,
+            };
+            generator_config.envelope = envelope;
+        }
     }
 }
