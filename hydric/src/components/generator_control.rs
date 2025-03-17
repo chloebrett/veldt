7392ffb74f50e9267d439be4d59ba7246e11e6da
Chloe Brett <chloebrett@users.noreply.github.com>
2025-03-17 16:22:00 +1100

    Use action dispatch for generator controls.

--- hydric/src/components/generator_control.rs
+++ hydric/src/components/generator_control.rs
@@ -1,23 +1,59 @@
+use crate::state::{Action, Store};
+use crate::widget::selectable_value;
 use egui::Ui;
-use shared::model::{SimpleWaveConfig, WaveType};
+use shared::model::{GeneratorType, WaveType};
+use strum::IntoEnumIterator;
 
-pub fn generator_control(config: &mut SimpleWaveConfig, ui: &mut Ui) {
+pub fn generator_control(store: &mut Store, ui: &mut Ui) {
+    let generator_type = store.project.generators[0].kind.clone();
+    let config = match generator_type {
+        GeneratorType::SimpleWave { config } => config,
+    };
+
     egui::ComboBox::from_label("Wave type")
         .selected_text(config.wave.to_string())
         .show_ui(ui, |ui| {
-            ui.selectable_value(&mut config.wave, WaveType::Sine, "Sine");
-            ui.selectable_value(&mut config.wave, WaveType::Square, "Square");
-            ui.selectable_value(&mut config.wave, WaveType::Saw, "Saw");
-            ui.selectable_value(&mut config.wave, WaveType::Triangle, "Triangle");
+            for wave in WaveType::iter() {
+                selectable_value(
+                    ui,
+                    |it| {
+                        it.map(|it| {
+                            store.dispatch(Action::SetWave {
+                                generator_index: 0,
+                                wave: it,
+                            })
+                        });
+                        config.wave
+                    },
+                    wave,
+                    wave.to_string(),
+                );
+            }
         });
     ui.add(
-        egui::Slider::new(&mut config.osc_count, 1..=24)
-            .text("Osc count")
-            .logarithmic(true),
+        egui::Slider::from_get_set(0.0..=24.0, |it| {
+            it.map(|it| {
+                store.dispatch(Action::SetOscCount {
+                    generator_index: 0,
+                    osc_count: it as u32,
+                })
+            });
+            config.osc_count as f64
+        })
+        .text("Osc count")
+        .fixed_decimals(0),
     );
     ui.add(
-        egui::Slider::new(&mut config.detune_cents, 0.0..=100.0)
-            .text("Osc detune")
-            .logarithmic(true),
+        egui::Slider::from_get_set(0.0..=100.0, |it| {
+            it.map(|it| {
+                store.dispatch(Action::SetDetuneCents {
+                    generator_index: 0,
+                    detune_cents: it as f32,
+                })
+            });
+            config.detune_cents as f64
+        })
+        .text("Osc detune")
+        .logarithmic(true),
     );
 }
