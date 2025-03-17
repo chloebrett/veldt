da8918c750c6d5ce628b7cb637a8b90cdd1b0d8f
Chloe Brett <chloebrett@users.noreply.github.com>
2025-03-17 14:53:12 +1100

    Actions wip

--- hydric/src/state/action.rs
+++ hydric/src/state/action.rs
@@ -1,6 +1,13 @@
 use shared::model::{Scale, ScaleValue};
+use shared::types::Beats;
 
 pub enum Action {
     SetKey(ScaleValue),
     SetScale(Scale),
+    SetBpm(Beats),
+    SetNote {
+        track_index: usize,
+        note_index: usize,
+        note: ScaleValue,
+    },
 }
