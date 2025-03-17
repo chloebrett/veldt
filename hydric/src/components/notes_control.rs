7942d06a71e3bd4b9dc91cfba947ad4c450f6ad0
Chloe Brett <chloebrett@users.noreply.github.com>
2025-03-17 14:53:12 +1100

    Actions wip

--- hydric/src/components/notes_control.rs
+++ hydric/src/components/notes_control.rs
@@ -7,27 +7,42 @@ use std::collections::BTreeSet;
 
 pub fn notes_control(app: &mut App, ui: &mut Ui, ctx: &Context) {
     let scale_options = create_scale_values(app.store.scale, app.store.key);
+
     let mut placed_notes: Vec<PlacedNote> = app.store.project.tracks[0]
         .notes
         .iter()
         .map(|placed_note| placed_note.clone())
         .collect();
+
     let mut track_length = 0.0;
+
     for i in 0..placed_notes.len() {
-        let placed_note = &mut placed_notes[i];
-        let note = &mut placed_note.note;
-        let scale_value = &mut note.pitch_name.scale_value;
+        let placed_note = &placed_notes[i];
+        let note = &placed_note.note;
+        let scale_value = &note.pitch_name.scale_value;
+
         egui::ComboBox::from_id_salt(i)
             .selected_text(scale_value.to_string())
             .show_ui(ui, |ui| {
                 for scale_note in scale_options.iter() {
-                    ui.selectable_value(scale_value, *scale_note, scale_note.to_string());
+                    selectable_value(
+                        ui,
+                        |it| {
+                            it.map(|it| app.store.dispatch(Action::SetNote(0, i, it)));
+                            scale_value
+                        },
+                        *scale_note,
+                        scale_note.to_string(),
+                    );
                 }
             });
+
         ui.add(egui::Slider::new(&mut note.pitch_name.octave, 0..=8).text("Octave"));
         ui.add(egui::Slider::new(&mut note.beats, 0.0..=10.0).text("Beats"));
+
         let mut offset_value = placed_note.offset.into();
         let response = ui.add(egui::Slider::new(&mut offset_value, 0.0..=16.0).text("Offset"));
+
         if !response.dragged() {
             placed_note.offset = OrderedFloat(offset_value);
         }
--- hydric/src/state/reducer.rs
+++ hydric/src/state/reducer.rs
@@ -1,8 +1,21 @@
 use super::{Action, Store};
+use shared::model::{Note, PlacedNote};
 
 pub fn reducer(store: &mut Store, action: Action) {
     match action {
         Action::SetKey(key) => store.key = key,
         Action::SetScale(scale) => store.scale = scale,
+        Action::SetBpm(bpm) => store.project.bpm = bpm,
+        Action::SetNote {
+            track_index,
+            note_index,
+            note,
+        } => {
+            let notes = &mut store.project.tracks[track_index].notes;
+            // TODO: don't do this, index by key in the set instead? Have a map too? Not sure yet.
+            let mut note: &mut PlacedNote = notes.iter().nth(note_index);
+
+            notes_vec[note_index].pitch_name.scale_value = note
+        }
     }
 }
