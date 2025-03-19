e4ca7c563d9b9ee3d68942c14700454b0a961067
elise <elise.dev.wheeler@gmail.com>
2025-03-19 20:58:43 +1100

    Make pitch value draggable.

--- hydric/src/components/note_display.rs
+++ hydric/src/components/note_display.rs
@@ -5,7 +5,6 @@ use shared::{
     model::{Note, PitchName, PlacedNote},
     types::PitchValue,
 };
-use web_sys::console;
 
 pub fn note_display(store: &Store, ui: &mut Ui) {
     Frame::canvas(ui.style()).show(ui, |ui| {
@@ -39,38 +38,38 @@ pub fn note_display(store: &Store, ui: &mut Ui) {
                 let y1 = y_size * pitch_proportion - 5.0;
                 let note_pos = Pos2 { x: x1, y: y1 };
                 // Closure to allow for moving notes in future.
-                let note_corner = || {
-                    let x2 = note_pos.x + note.note.beats * inv_project_length * x_size;
-                    let y2 = note_pos.y + 5.0;
-                    Pos2 { x: x2, y: y2 }
-                };
+                let x2 = note_pos.x + note.note.beats * inv_project_length * x_size;
+                let y2 = note_pos.y + 5.0;
+                let note_corner = Pos2 { x: x2, y: y2 };
                 let min_corner = to_screen.transform_pos(note_pos);
-                let max_corner = to_screen.transform_pos(note_corner());
+                let max_corner = to_screen.transform_pos(note_corner);
                 let note_rect = Rect::from_min_max(min_corner, max_corner);
                 let note_id = response.id.with(i);
                 let note_response = ui.interact(note_rect, note_id, Sense::drag());
                 let note_delta = note_response.drag_delta();
                 let scaled_note_delta = Vec2 {
                     x: note_delta.x * inv_x_size * project_length,
-                    y: 0.0, //y: note_delta.y * inv_y_size * max_pitch_value as f32,
+                    y: -note_delta.y * inv_y_size * max_pitch_value as f32,
                 };
                 let offset_delta = ordered_float::OrderedFloat(scaled_note_delta.x);
-                let pitch_delta = Into::<PitchName>::into(scaled_note_delta.y.round() as i32);
-
+                let pitch_delta = Into::<PitchValue>::into(scaled_note_delta.y.round() as i32);
                 *note = PlacedNote {
                     note: Note {
-                        pitch_name: note.note.pitch_name + pitch_delta.into(),
+                        pitch_name: note.note.pitch_name + pitch_delta,
                         beats: note.note.beats,
                     },
                     offset: note.offset + offset_delta,
                 };
                 let sel = Selector::Note(0, i);
+                store.dispatch(&sel, Action::SetNoteOctave(note.note.pitch_name.octave));
+                store.dispatch(
+                    &sel,
+                    Action::SetNoteScaleValue(note.note.pitch_name.scale_value),
+                );
                 store.dispatch(&sel, Action::SetNoteOffset(*note.offset));
-                console::log_1(&format!("{:?}", store.get().project.tracks[0].notes[0]).into());
                 Shape::rect_filled(note_rect, CornerRadius::same(1), Color32::WHITE)
             })
             .collect();
-
         painter.extend(note_shapes);
         response
     });
