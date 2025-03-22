6accd182176eeb73681c2a118994cf6269acdfef
Chloe Brett <chloebrett@users.noreply.github.com>
2025-03-22 16:32:25 +1100

    Automate more proto conversions.

--- shared/src/model/generator.rs
+++ shared/src/model/generator.rs
@@ -2,45 +2,42 @@ use crate::model::{AdsrEnvelope, WaveType};
 use crate::pmodel::SimpleWaveProto;
 use crate::pmodel::{
     GeneratorInstanceProto, GeneratorMetaProto, SimpleWaveConfigProto,
-    generator_instance_proto::Kind,
+    generator_instance_proto::Kind as GeneratorTypeProto,
 };
 use crate::types::Volume;
 use local_macro::{FromProto, IntoProto};
 
 type GeneratorInstanceId = usize;
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, FromProto, IntoProto)]
 pub struct GeneratorInstance {
+    #[proto_type_u32]
     pub id: GeneratorInstanceId,
 
+    #[proto_optional]
     pub kind: GeneratorType,
 
+    #[proto_optional]
     pub meta: GeneratorMeta,
 }
 
-impl From<GeneratorInstanceProto> for GeneratorInstance {
-    fn from(item: GeneratorInstanceProto) -> Self {
-        GeneratorInstance {
-            id: item.id as usize,
-            meta: item.meta.unwrap().into(),
-            kind: match item.kind.unwrap() {
-                Kind::SimpleWave(config) => GeneratorType::SimpleWave {
-                    config: config.config.unwrap().into(),
-                },
-            },
+impl From<GeneratorType> for GeneratorTypeProto {
+    fn from(item: GeneratorType) -> GeneratorTypeProto {
+        match item {
+            GeneratorType::SimpleWave { config } => {
+                GeneratorTypeProto::SimpleWave(SimpleWaveProto {
+                    config: Some(config.into()),
+                })
+            }
         }
     }
 }
 
-impl From<GeneratorInstance> for GeneratorInstanceProto {
-    fn from(item: GeneratorInstance) -> Self {
-        GeneratorInstanceProto {
-            id: item.id as u32,
-            meta: Some(item.meta.into()),
-            kind: match item.kind {
-                GeneratorType::SimpleWave { config } => Some(Kind::SimpleWave(SimpleWaveProto {
-                    config: Some(config.into()),
-                })),
+impl From<GeneratorTypeProto> for GeneratorType {
+    fn from(item: GeneratorTypeProto) -> GeneratorType {
+        match item {
+            GeneratorTypeProto::SimpleWave(config) => GeneratorType::SimpleWave {
+                config: config.config.unwrap().into(),
             },
         }
     }
