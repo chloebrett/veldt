4b44dd96c06a3081d9e71920f80066a0f7c759d0
Chloe Brett <chloebrett@users.noreply.github.com>
2025-03-27 21:18:30 +1100

    Split StoreData out of store.rs.

--- hydric/src/state/store.rs
+++ hydric/src/state/store.rs
@@ -1,16 +1,6 @@
-use super::UndoStack;
-use super::{Action, Selector};
-use ordered_float::OrderedFloat;
-use poll_promise::Promise;
+use super::{UndoStack, StoreData, Action, Selector};
 use shared::logger::log;
-use shared::model::{
-    AdsrEnvelope, DelayConfig, Effect, EffectInstance, EffectMeta, EqConfig, EqType,
-    GeneratorInstance, GeneratorMeta, GeneratorType, MixerChannel, Note, PitchName, PlacedNote,
-    Project, Sample, Scale, ScaleValue, SimpleWaveConfig, Track, WaveType,
-};
-use shared::types::Volume;
 use std::cell::RefCell;
-use std::rc::Rc;
 
 enum UndoRedoType {
     Undo,
@@ -38,21 +28,6 @@ pub struct Store {
     /// The pending undo/redo, if one is pending. This will be flushed on the next frame,
     /// to avoid mutating the StoreData mid-frame.
     pending_undo_redo: Option<UndoRedoType>,
-}
-
-#[derive(Clone)]
-pub struct StoreData {
-    pub project: Project,
-    pub key: ScaleValue,
-    pub scale: Scale,
-    pub volume: Volume,
-    // TODO: call this "project_list"?
-    pub track_list: Vec<String>,
-    pub load_track_name: Option<String>,
-    pub save_track_promise: Rc<Option<Promise<Option<()>>>>,
-    pub track_list_promise: Rc<Option<Promise<Option<Vec<String>>>>>,
-    pub load_track_promise: Rc<Option<Promise<Option<Track>>>>,
-    pub load_sample_promise: Rc<Option<Promise<Option<Sample>>>>,
 }
 
 impl Store {
@@ -111,77 +86,3 @@ impl Store {
     }
 }
 
-impl Default for StoreData {
-    fn default() -> Self {
-        StoreData {
-            project: Project {
-                name: "My Project".to_string(),
-                tracks: vec![Track {
-                    notes: vec![PlacedNote {
-                        note: Note {
-                            pitch_name: PitchName {
-                                scale_value: ScaleValue::A,
-                                octave: 4,
-                            },
-                            beats: 1.0,
-                        },
-                        offset: OrderedFloat(0.0),
-                    }],
-                }],
-                // TODO: use track placements
-                track_placements: vec![],
-                samples: vec![],
-                generators: vec![GeneratorInstance {
-                    id: 0,
-                    kind: GeneratorType::SimpleWave {
-                        config: SimpleWaveConfig {
-                            wave: WaveType::Sine,
-                            envelope: AdsrEnvelope {
-                                attack: 0.1,
-                                decay: 0.1,
-                                sustain: 0.8,
-                                release: 0.1,
-                            },
-                            osc_count: 4,
-                            detune_cents: 5.0,
-                        },
-                    },
-                    meta: GeneratorMeta { volume: 1.0 },
-                }],
-                mixer: vec![MixerChannel {
-                    effects: vec![
-                        EffectInstance {
-                            effect: Effect::SimpleEq {
-                                config: EqConfig {
-                                    kind: EqType::SimpleResonator,
-                                    fc: 1000.0,
-                                    q: 1.0,
-                                },
-                            },
-                            meta: EffectMeta { id: 0, wet: 1.0 },
-                        },
-                        EffectInstance {
-                            effect: Effect::SimpleDelay {
-                                config: DelayConfig {
-                                    amplitude: 0.5,
-                                    delay_ms: 250.0,
-                                },
-                            },
-                            meta: EffectMeta { id: 1, wet: 0.5 },
-                        },
-                    ],
-                }],
-                bpm: 120.0,
-            },
-            volume: 1.0,
-            key: ScaleValue::A,
-            scale: Scale::Chromatic,
-            track_list: vec![],
-            save_track_promise: Rc::new(None),
-            track_list_promise: Rc::new(None),
-            load_track_promise: Rc::new(None),
-            load_sample_promise: Rc::new(None),
-            load_track_name: None,
-        }
-    }
-}
