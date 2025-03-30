ab14852e478f79766b77698afdd18613692ea06d
elise <135685125+elisedemarie@users.noreply.github.com>
2025-03-30 16:54:37 +1100

    Test wave_type convertion to proto.

--- shared/src/model/wave_type.rs
+++ shared/src/model/wave_type.rs
@@ -23,3 +23,24 @@ pub enum WaveType {
     Saw,
     Triangle, // TODO: also add a generator for white noise - but it's not constrained by freq.
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn convert_from_wave_type_to_proto_and_back() {
+        let wave_type = WaveType::Sine;
+        let proto: WaveTypeProto = wave_type.into();
+        let result: WaveType = proto.into();
+        assert_eq!(wave_type, result);
+    }
+
+    #[test]
+    fn convert_from_proto_to_wave_type_and_back() {
+        let proto = WaveTypeProto::SineWaveType;
+        let wave_type: WaveType = proto.into();
+        let result: WaveTypeProto = wave_type.into();
+        assert_eq!(proto, result);
+    }
+}
