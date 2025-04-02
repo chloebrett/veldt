651af4f4dc13a3e40e93473a9bbeafd0a221ae94
Chloe Brett <6735055+chloebrett@users.noreply.github.com>
2025-04-03 09:09:31 +1100

    Revert "WIP: generator node."

--- mesic/src/graph/mod.rs
+++ mesic/src/graph/mod.rs
@@ -5,7 +5,6 @@ mod amp_node;
 mod buffer_node;
 mod compressor_node;
 mod eq_node;
-mod generator_node;
 mod mixer_node;
 mod render_graph;
 
@@ -13,7 +12,6 @@ pub use amp_node::*;
 pub use buffer_node::*;
 pub use compressor_node::*;
 pub use eq_node::*;
-pub use generator_node::*;
 pub use mixer_node::*;
 pub use render_graph::*;
 
