9e48fe91c7292d129f69a73f7370739d8c318361
bowendong1234 <bdon0009@student.monash.edu>
2025-04-05 14:37:46 +1100

    Formatting changes after running pre-commit script (properly this time)

--- mesic/src/effect/eq/mod.rs
+++ mesic/src/effect/eq/mod.rs
@@ -4,20 +4,20 @@ mod bps_basic;
 mod filter;
 mod lhp_first_order;
 mod lhp_second_order;
+mod lhp_second_order_lr;
 mod low_high;
 mod resonator_sa;
 mod resonator_simple;
-mod lhp_second_order_lr;
 
 use apf_first_order::*;
 use apf_second_order::*;
 use bps_basic::*;
 use lhp_first_order::*;
 use lhp_second_order::*;
+use lhp_second_order_lr::*;
 use low_high::LowHigh;
 use resonator_sa::*;
 use resonator_simple::*;
-use lhp_second_order_lr::*;
 use shared::model::{EqConfig, EqType};
 
 pub trait ApplyFilter {
@@ -36,8 +36,12 @@ pub fn eq_filter(config: &EqConfig) -> Box<dyn ApplyFilter> {
         EqType::FirstOrderAllPass => Box::new(apf_first_order(config)),
         EqType::SecondOrderAllPass => Box::new(apf_second_order(config)),
         EqType::SimpleSecondOrderBandStop => Box::new(band_stop_basic(config)),
-        EqType::LinkwitzRileySecondOrderLowPass => Box::new(lhp_second_order_lr(config, LowHigh::Low)),
-        EqType::LinkwitzRileySecondOrderHighPass => Box::new(lhp_second_order_lr(config, LowHigh::High)),
+        EqType::LinkwitzRileySecondOrderLowPass => {
+            Box::new(lhp_second_order_lr(config, LowHigh::Low))
+        }
+        EqType::LinkwitzRileySecondOrderHighPass => {
+            Box::new(lhp_second_order_lr(config, LowHigh::High))
+        }
         _ => panic!("EQ type not implemented!"),
     }
 }
