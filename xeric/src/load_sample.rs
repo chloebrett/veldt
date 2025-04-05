88bf30db2bafd34f81a4014a4bfa26a7e570ac55
elise <135685125+elisedemarie@users.noreply.github.com>
2025-04-05 15:11:11 +1100

    Add tests to load_sample.

--- xeric/src/load_sample.rs
+++ xeric/src/load_sample.rs
@@ -31,7 +31,7 @@ impl LoadSample for MyLoadSample {
         file_path.pop(); // pop '/xeric'
         file_path.push("assets");
         file_path.push("samples");
-        file_path.push(filename);
+        file_path.push(filename.clone());
         log(&format!(
             "Loading sample from path: {}",
             file_path.clone().display()
@@ -40,18 +40,64 @@ impl LoadSample for MyLoadSample {
         // TODO: reading this seems to load at half the speed.
         // Perhaps the sample rate needs to be adjusted?
         // Either way, a bit weird and should be fixed.
-        let mut reader = hound::WavReader::open(file_path).unwrap();
-        // TODO: handle 16-bit audio appropriately.
-        let data: Vec<f32> = reader
-            .samples::<i32>()
-            .map(|it| to_f32(it.unwrap()))
-            .collect();
-        let sample = Sample {
-            data,
-            sample_rate: reader.spec().sample_rate as f32,
-        };
-        Ok(tonic::Response::new(LoadSampleReply {
-            sample: Some(sample.into()),
-        }))
+        let reader_response = hound::WavReader::open(file_path);
+        if let Ok(mut reader) = reader_response {
+            // TODO: handle 16-bit audio appropriately.
+            let data: Vec<f32> = reader
+                .samples::<i32>()
+                .map(|it| to_f32(it.unwrap()))
+                .collect();
+            let sample = Sample {
+                data,
+                sample_rate: reader.spec().sample_rate as f32,
+            };
+            Ok(tonic::Response::new(LoadSampleReply {
+                sample: Some(sample.into()),
+            }))
+        } else {
+            Err(tonic::Status::invalid_argument(format!(
+                "File {} could not be read.",
+                filename
+            )))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn load_sample() {
+        // ARRANGE
+        let my_load_sample = MyLoadSample;
+        let sample_name = "89 BPM F# Minor.wav";
+        let load_request = tonic::Request::new(LoadSampleRequest {
+            filename: sample_name.into(),
+        });
+
+        // ACT
+        let load_request = my_load_sample.load_sample(load_request).await;
+
+        // ASSERT
+        // TODO more meaningful check of return.
+        assert!(load_request.is_ok())
+    }
+
+    #[tokio::test]
+    async fn load_invalid_file_name_fails() {
+        // ARRANGE
+        let my_load_sample = MyLoadSample;
+        let sample_name = "test.wav";
+        let load_request = tonic::Request::new(LoadSampleRequest {
+            filename: sample_name.into(),
+        });
+
+        // ACT
+        let load_request = my_load_sample.load_sample(load_request).await;
+
+        // ASSERT
+        // TODO more meaningful check of return.
+        assert!(load_request.is_err())
     }
 }
