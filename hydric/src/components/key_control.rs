e5a82a6486d8cce0c8a44b04c4ba5f44581b52aa
elise <135685125+elisedemarie@users.noreply.github.com>
2025-04-09 20:49:41 +1000

    Fix bug with selector.

--- hydric/src/components/key_control.rs
+++ hydric/src/components/key_control.rs
@@ -8,15 +8,12 @@ use strum::IntoEnumIterator;
 
 pub struct KeyControl {
     key: ScaleValue,
-    scale: Scale
+    scale: Scale,
 }
 
 impl KeyControl {
     pub fn new(key: ScaleValue, scale: Scale) -> Self {
-        KeyControl {
-            key,
-            scale
-        }
+        KeyControl { key, scale }
     }
 }
 
@@ -24,26 +21,26 @@ impl View for KeyControl {
     fn ui(&self, store: &Store, ui: &mut Ui) {
         let KeyControl { key, scale } = *self;
         ComboBox::from_label("Key")
-        .selected_text(key.to_string())
-        .show_ui(ui, |ui| {
-            for scale_note in ScaleValue::iter() {
-                selectable_value(
-                    ui,
-                    get_set(key, |it| store.dispatchr(Action::SetKey(it))),
-                    scale_note,
-                    scale_note.to_string(),
-                );
-            }
-        });
+            .selected_text(key.to_string())
+            .show_ui(ui, |ui| {
+                for scale_note in ScaleValue::iter() {
+                    selectable_value(
+                        ui,
+                        get_set(key, |it| store.dispatchr(Action::SetKey(it))),
+                        scale_note,
+                        scale_note.to_string(),
+                    );
+                }
+            });
         ComboBox::from_label("Scale")
             .selected_text(scale.to_string())
             .show_ui(ui, |ui| {
-                for scale in Scale::iter() {
+                for scale_option in Scale::iter() {
                     selectable_value(
                         ui,
                         get_set(scale, |it| store.dispatchr(Action::SetScale(it))),
-                        scale,
-                        scale.to_string(),
+                        scale_option,
+                        scale_option.to_string(),
                     );
                 }
             });
