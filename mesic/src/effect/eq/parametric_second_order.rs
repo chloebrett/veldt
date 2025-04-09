a749e07ade36dac3ccf8fc31f2b46007021bf31d
maddyyy0110 <tpra0008@student.monash.edu>
2025-04-09 20:21:53 +1000

    Resolving Chloe's  points of improvement.

--- mesic/src/effect/eq/parametric_second_order.rs
+++ mesic/src/effect/eq/parametric_second_order.rs
@@ -1,19 +1,20 @@
 use super::filter::{SecondOrderFilter, SecondOrderFilterConfig};
 use crate::consts::SAMPLE_RATE;
 use shared::model::EqConfig;
-use std::f32::consts::PI;
+use std::f32::consts::TAU;
 
 pub fn parametric_non_constant_q(config: &EqConfig) -> SecondOrderFilter {
     let fs = SAMPLE_RATE as f32;
 
-    let thetac: f32 = 2.0 * PI * config.fc / fs;
+    let thetac: f32 = TAU * config.fc / fs;
 
     let mu: f32 = 10.0_f32.powf(config.gain / 20.0);
 
     let zeta: f32 = 4.0 / (1.0 + mu);
 
-    let beta: f32 = 0.5 * (1.0 - zeta * (thetac / (2.0 * config.q)).tan())
-        / (1.0 + zeta * (thetac / (2.0 * config.q)).tan());
+    let tan_constant: f32 = (thetac / (2.0 * config.q)).tan();
+
+    let beta: f32 = 0.5 * (1.0 - zeta * tan_constant) / (1.0 + zeta * tan_constant);
 
     let gamma: f32 = (0.5 + beta) * thetac.cos();
 
@@ -24,6 +25,7 @@ pub fn parametric_non_constant_q(config: &EqConfig) -> SecondOrderFilter {
     let b2: f32 = 2.0 * beta;
 
     // We may need to use this at some point, currently no handing for it :)
+
     // let c0:f32 = mu - 1.0;
     // let d0:f32 = 1;
 
