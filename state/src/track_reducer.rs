149fdf2016770c675a40161d7b246c59700f3c88
elise <135685125+elisedemarie@users.noreply.github.com>
2025-04-11 07:13:32 +1000

    Mutate note offset when changing track offset.

--- state/src/track_reducer.rs
+++ state/src/track_reducer.rs
@@ -23,6 +23,9 @@ pub fn track_reducer(track: &mut Track, action: &Action) -> Action {
         }
         Action::SetTrackOffset(offset) => {
             let prev = track.offset;
+            for note in track.notes.iter_mut() {
+                note.offset += *offset-*prev;
+            }
             track.offset = OrderedFloat(*offset);
             Action::SetTrackOffset(*prev)
         }
