85054a0c0b559be9972922cefe030affa47fac79
Chloe Brett <chloebrett@users.noreply.github.com>
2025-04-13 10:42:07 +1000

    Replace sliders with the custom slider widget.

--- hydric/src/widget/slider.rs
+++ hydric/src/widget/slider.rs
@@ -12,6 +12,7 @@ pub fn slider<T: PartialEq + Clone + Into<f64> + From<f64>, F: Fn(T), G: Fn()>(
 ) {
     slider_internal(
         ui, label, value, setter, range, on_release, /* logarithmic= */ false,
+        /* fixed_decimals= */ None,
     );
 }
 
@@ -25,6 +26,27 @@ pub fn log_slider<T: PartialEq + Clone + Into<f64> + From<f64>, F: Fn(T), G: Fn(
 ) {
     slider_internal(
         ui, label, value, setter, range, on_release, /* logarithmic= */ true,
+        /* fixed_decimals= */ None,
+    );
+}
+
+pub fn int_slider<T: PartialEq + Clone + Into<f64> + From<f64>, F: Fn(T), G: Fn()>(
+    ui: &mut Ui,
+    label: &str,
+    value: T,
+    setter: F,
+    range: RangeInclusive<i32>,
+    on_release: G,
+) {
+    slider_internal(
+        ui,
+        label,
+        value,
+        setter,
+        (*range.start() as f32).into()..=(*range.end() as f32).into(),
+        on_release,
+        /* logarithmic= */ true,
+        /* fixed_decimals= */ Some(0),
     );
 }
 
@@ -36,12 +58,18 @@ fn slider_internal<T: PartialEq + Clone + Into<f64> + From<f64>, F: Fn(T), G: Fn
     range: RangeInclusive<f64>,
     on_release: G,
     logarithmic: bool,
+    fixed_decimals: Option<usize>,
 ) {
-    let response = ui.add(
+    let mut slider =
         egui::Slider::from_get_set(range, get_set(value.into(), |it| setter(it.into())))
             .text(label)
-            .logarithmic(logarithmic),
-    );
+            .logarithmic(logarithmic);
+
+    if let Some(fixed_decimals) = fixed_decimals {
+        slider = slider.fixed_decimals(fixed_decimals);
+    }
+
+    let response = ui.add(slider);
 
     if response.drag_stopped() || response.lost_focus() {
         on_release();
