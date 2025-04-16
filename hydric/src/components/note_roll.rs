66a157fbc3ea57ec42b9f82658e2c4a22b0c5306
elise <135685125+elisedemarie@users.noreply.github.com>
2025-04-16 17:55:28 +1000

    Move note window state to ui and opennable by NoteRoll

--- hydric/src/components/note_roll.rs
+++ hydric/src/components/note_roll.rs
@@ -1,6 +1,6 @@
 use state::{Action, Selector, Store};
 
-use egui::{Color32, Pos2, Rect, ScrollArea, Ui, pos2, vec2};
+use egui::{Color32, Id, Pos2, Rect, ScrollArea, Ui, pos2, vec2};
 use shared::{
     model::{Note, PitchName, PlacedNote, Scale, ScaleValue},
     types::PitchValue,
@@ -103,7 +103,10 @@ impl View for NoteRoll {
                     store.dispatch(&Selector::Note(track_index, note_index), action)
                 };
                 let on_release = || store.dispatchr(Action::Release);
-                let on_click = |_ui: &mut Ui, _index: usize| {};
+                let on_click = |ui: &mut Ui, _index: usize| {
+                    let note_id = Id::new("note_window");
+                    ui.data_mut(|data| data.insert_temp(note_id, true))
+                };
                 ui.horizontal(|ui| {
                     Piano::new(max_note, min_note - 1).ui(store, ui);
                     ui.add(
--- hydric/src/components/window_toggle.rs
+++ hydric/src/components/window_toggle.rs
@@ -8,7 +8,6 @@ pub fn window_toggle_button(toggle_value: bool, ui: &mut Ui, toggle_name: &str) 
 pub fn toggle_window_panel(window_state: &mut WindowState, ui: &mut Ui) {
     ui.horizontal(|ui| {
         window_state.mixer.visible = window_toggle_button(window_state.mixer.visible, ui, "Mixer");
-        window_state.manual_notes = window_toggle_button(window_state.manual_notes, ui, "Notes");
         window_state.generator_list =
             window_toggle_button(window_state.generator_list, ui, "Generators");
         window_state.scale = window_toggle_button(window_state.scale, ui, "Scale");
