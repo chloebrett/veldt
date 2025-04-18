a5efcabd83fbfe9073717da1fc7919700a564461
bowendong1234 <bdon0009@student.monash.edu>
2025-04-18 11:33:45 +1000

    Added wave visualister snd oscillator sub components to mod

--- hydric/src/components/generator/mod.rs
+++ hydric/src/components/generator/mod.rs
@@ -2,8 +2,15 @@ mod envelope_view;
 mod generator_control;
 mod generators_control;
 mod simple_wave_control;
+mod subsynth_control;
+mod simple_wave_visualiser;
+mod subsynth_oscillator;
 
 pub use envelope_view::*;
 pub use generator_control::*;
 pub use generators_control::*;
+pub use simple_wave_visualiser::*;
+pub use subsynth_oscillator::*;
 use simple_wave_control::*;
+use subsynth_control::*;
+
