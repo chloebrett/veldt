e923aad840318cf8736f2d46d8a9d64bb446fa08
bowendong1234 <bdon0009@student.monash.edu>
2025-04-18 23:24:40 +1000

    Updated styling and added stacking ui elements

--- hydric/src/components/generator/subsynth_oscillator.rs
+++ hydric/src/components/generator/subsynth_oscillator.rs
@@ -1,7 +1,7 @@
 use super::SimpleWaveVisualiser;
-use crate::widget::knob;
+use crate::widget::{knob, int_slider, get_set};
 use eframe::egui;
-use egui::{Color32, ComboBox, Slider};
+use egui::{Color32, ComboBox};
 use shared::model::WaveType;
 use state::Action;
 
@@ -12,6 +12,7 @@ pub enum OscillatorId {
     Osc3,
 }
 
+// TODO fix this make sure its hooked up to config instead
 pub struct SubsynthOscillator {
     id: OscillatorId,
     wave_type: WaveType,
@@ -37,10 +38,10 @@ impl SubsynthOscillator {
             line_color,
             fill_color,
             // Default styling
-            border_color: Color32::GRAY, // change to black to match figma once the rest is implemented
+            border_color: Color32::from_rgb(60, 60, 60),
             border_width: 1.0,
             border_radius: 8.0,
-            background_color: Color32::from_rgb(30, 30, 30),
+            background_color: Color32::from_rgb(50, 50, 50),
         }
     }
 
@@ -54,91 +55,145 @@ impl SubsynthOscillator {
             .fill(self.background_color)
             .stroke(egui::Stroke::new(self.border_width, self.border_color))
             .corner_radius(self.border_radius)
-            .inner_margin(10.0);
+            .inner_margin(6.0);
 
         frame
             .show(ui, |ui| {
                 ui.horizontal(|ui| {
-                    ui.vertical(|ui| {
-                        // Wave type dropdown
-                        ui.horizontal(|ui| {
-                            let wave_type_str = match self.wave_type {
-                                WaveType::Sine => "Sine",
-                                WaveType::Square => "Square",
-                                WaveType::Saw => "Saw",
-                                WaveType::Triangle => "Triangle",
-                            };
 
-                            ComboBox::from_label("")
-                                .selected_text(wave_type_str)
-                                .show_ui(ui, |ui| {
-                                    ui.selectable_value(
-                                        &mut self.wave_type,
-                                        WaveType::Sine,
-                                        "Sine",
-                                    );
-                                    ui.selectable_value(
-                                        &mut self.wave_type,
-                                        WaveType::Square,
-                                        "Square",
-                                    );
-                                    ui.selectable_value(&mut self.wave_type, WaveType::Saw, "Saw");
-                                    ui.selectable_value(
-                                        &mut self.wave_type,
-                                        WaveType::Triangle,
-                                        "Triangle",
-                                    );
-                                });
+                    // osc selection frame shows dropdown and visualiser for wave type
+                    let osc_selection_frame = egui::Frame::new()
+                        .fill(Color32::from_rgb(30, 30, 30))
+                        .corner_radius(8.0)
+                        .inner_margin(10.0);
+                    osc_selection_frame.show(ui, |ui| {
+                        ui.vertical(|ui| {
+                            ui.horizontal(|ui| {
+                                let wave_type_str = match self.wave_type {
+                                    WaveType::Sine => "Sine",
+                                    WaveType::Square => "Square",
+                                    WaveType::Saw => "Saw",
+                                    WaveType::Triangle => "Triangle",
+                                };
+    
+                                ComboBox::from_label("")
+                                    .selected_text(wave_type_str)
+                                    .show_ui(ui, |ui| {
+                                        ui.selectable_value(
+                                            &mut self.wave_type,
+                                            WaveType::Sine,
+                                            "Sine",
+                                        );
+                                        ui.selectable_value(
+                                            &mut self.wave_type,
+                                            WaveType::Square,
+                                            "Square",
+                                        );
+                                        ui.selectable_value(&mut self.wave_type, WaveType::Saw, "Saw");
+                                        ui.selectable_value(
+                                            &mut self.wave_type,
+                                            WaveType::Triangle,
+                                            "Triangle",
+                                        );
+                                    });
+                            });
+                            ui.add_space(10.0);
+                            let visualiser = SimpleWaveVisualiser::new(
+                                self.wave_type,
+                                self.line_color,
+                                self.fill_color,
+                            );
+    
+                            visualiser.show(ui);
                         });
-                        ui.add_space(10.0);
-                        let visualiser = SimpleWaveVisualiser::new(
-                            self.wave_type,
-                            self.line_color,
-                            self.fill_color,
-                        );
-
-                        visualiser.show(ui);
                     });
-                    ui.add_space(14.0);
-                    ui.vertical(|ui| {
-                        knob(
-                            ui,
-                            "Volume",
-                            0.0, // placeholder value fix this to be config instead
-                            |it| dispatch(Action::SetGeneratorVolume(it)), // this action is prob not the right action idk
-                            0.0..=1.0,
-                            &on_release,
-                        );
-                        ui.add_space(3.0);
 
-                        knob(
-                            ui,
-                            "Pan",
-                            0.0, // placeholder value
-                            |it| dispatch(Action::SetGeneratorVolume(it)), // TODO fix action its a placeholder rn
-                            0.0..=1.0,
-                            &on_release,
-                        );
-                        ui.add_space(3.0);
+                    // volume, pan, tuning section
+                    let four_knob_frame = egui::Frame::new()
+                        .fill(Color32::from_rgb(30, 30, 30))
+                        .corner_radius(8.0)
+                        .inner_margin(10.0);
+                    four_knob_frame.show(ui, |ui| {
+                        ui.vertical(|ui| {
+                            knob(
+                                ui,
+                                "Volume",
+                                0.0, // placeholder value fix this to be config instead
+                                |it| dispatch(Action::SetGeneratorVolume(it)), // this action is prob not the right action idk
+                                0.0..=1.0,
+                                &on_release,
+                            );
+                            ui.add_space(6.0);
+    
+                            knob(
+                                ui,
+                                "Pan",
+                                0.0, // placeholder value
+                                |it| dispatch(Action::SetGeneratorVolume(it)), // TODO fix action its a placeholder rn
+                                0.0..=1.0,
+                                &on_release,
+                            );
+                            ui.add_space(6.0);
+    
+                            knob(
+                                ui,
+                                "Coarse",
+                                0.0, // placeholder value
+                                |it| dispatch(Action::SetGeneratorVolume(it)), // TODO fix action its a placeholder rn
+                                0.0..=1.0,
+                                &on_release,
+                            );
+                            ui.add_space(6.0);
+    
+                            knob(
+                                ui,
+                                "Fine",
+                                0.0, // placeholder value
+                                |it| dispatch(Action::SetGeneratorVolume(it)), // TODO fix action its a placeholder rn
+                                0.0..=1.0,
+                                &on_release,
+                            );
+                        });
+                    });
 
-                        knob(
-                            ui,
-                            "Coarse",
-                            0.0, // placeholder value
-                            |it| dispatch(Action::SetGeneratorVolume(it)), // TODO fix action its a placeholder rn
-                            0.0..=1.0,
-                            &on_release,
-                        );
-                        ui.add_space(3.0);
+                    // stacking section
+                    let stacking_frame = egui::Frame::new()
+                        .fill(Color32::from_rgb(30, 30, 30))
+                        .corner_radius(8.0)
+                        .inner_margin(10.0);
+                    stacking_frame.show(ui, |ui| {
+                        ui.vertical(|ui| {
+                            ui.label("Stacking");
+                            ui.add_space(4.0);
+                            int_slider(
+                                ui,
+                                "Unison",
+                                1.0 as f64, // TODO placeholder value remember to hook it up to config instead
+                                |it| dispatch(Action::SetOscCount(it as u32)), // need diff action probably
+                                1..=24,
+                                &on_release,
+                            );
+                            ui.add_space(6.0);
+                            knob(
+                                ui,
+                                "Osc detune",
+                                0.0, // TODO placeholder value remember to hook it up to config instead
+                                |it| dispatch(Action::SetDetuneCents(it)), // need diff action probably
+                                0.0..=100.0,
+                                &on_release,
+                            );
+                        });
 
-                        knob(
-                            ui,
-                            "Fine",
-                            0.0, // placeholder value
-                            |it| dispatch(Action::SetGeneratorVolume(it)), // TODO fix action its a placeholder rn
-                            0.0..=1.0,
-                            &on_release,
-                        );
+                        // LFO and ENV selection section
+                        let lfo_env_frame = egui::Frame::new()
+                            .fill(Color32::from_rgb(30, 30, 30))
+                            .corner_radius(8.0)
+                            .inner_margin(10.0);
+                        lfo_env_frame.show(ui, |ui| {
+                            ui.horizontal(|ui| {
+                                // TODO add LFOS and ENV drop downs + knobs
+                            })
+                        })
                     });
                 });
             })
