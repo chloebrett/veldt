0adfa2b2cf70ad537430c12f489e42adacdfccbd
elise <135685125+elisedemarie@users.noreply.github.com>
2025-04-18 17:50:26 +1000

    Allow graph to render multiple tracks.

--- mesic/src/graph/render_graph.rs
+++ mesic/src/graph/render_graph.rs
@@ -5,7 +5,7 @@ use super::{
 use crate::consts::SAMPLE_RATE;
 use crate::effect::eq_filter;
 use dasp_frame::Stereo;
-use dasp_graph::{BoxedNodeSend, Buffer, Node, NodeData};
+use dasp_graph::{BoxedNodeSend, Buffer, Node, NodeData, node::SumBuffers};
 use petgraph::stable_graph::NodeIndex;
 use shared::model::{Effect, EffectInstance};
 
@@ -15,31 +15,56 @@ pub struct RenderGraph {
     sample_count: usize,
     output_node_index: NodeIndex,
     processor: Processor,
+    generator_indexes: Vec<NodeIndex>,
 
     // For iteration.
     processed_samples_count: usize,
 }
 
-impl RenderGraph {
-    pub fn new(graph: Graph, sample_count: usize, output_node_index: NodeIndex) -> Self {
+impl Default for RenderGraph {
+    fn default() -> Self {
+        let mut graph = make_graph();
+        // Set a Buffer Summer as output all inputs on the graph.
+        let output_node_index = graph.add_node(NodeData::new2(BoxedNodeSend::new(SumBuffers {})));
         RenderGraph {
             graph,
-            sample_count,
+            sample_count: 0,
             output_node_index,
+            generator_indexes: vec![],
             processor: make_processor(),
             processed_samples_count: 0,
         }
+    }
+}
+
+impl RenderGraph {
+    pub fn add_generator(&mut self, node: impl Node + 'static + Send, sample_count: usize) {
+        let node_index = self
+            .graph
+            .add_node(NodeData::new2(BoxedNodeSend::new(node)));
+        // Keep track of node index to easily connect to Effects and Mixers
+        self.generator_indexes.push(node_index);
+        self.sample_count = usize::max(self.sample_count, sample_count);
+        self.graph.add_edge(node_index, self.output_node_index, ());
     }
 
     pub fn add_node(&mut self, node: impl Node + 'static + Send) {
         let node_index = self
             .graph
             .add_node(NodeData::new2(BoxedNodeSend::new(node)));
-        self.graph.add_edge(self.output_node_index, node_index, ());
-        self.output_node_index = node_index;
+        self.graph.add_edge(node_index, self.output_node_index, ());
     }
 
-    pub fn add_effect_with_mixer(&mut self, effect: EffectInstance) {
+    pub fn add_output_node(&mut self, node: impl Node + 'static + Send) {
+        let node_index = self
+            .graph
+            .add_node(NodeData::new2(BoxedNodeSend::new(node)));
+        self.graph.add_edge(self.output_node_index, node_index, ());
+        // Set node as new output
+        self.output_node_index = node_index
+    }
+
+    pub fn add_effect_with_mixer(&mut self, effect: EffectInstance, generator_index: usize) {
         let effect_node = match effect.effect {
             Effect::SimpleEq { config } => BoxedNodeSend::new(EqNode {
                 filter_left: eq_filter(&config),
@@ -62,7 +87,10 @@ impl RenderGraph {
             mute: effect.meta.mute,
         };
 
-        let dry = self.output_node_index;
+        let dry = *self
+            .generator_indexes
+            .get(generator_index)
+            .expect("Should have been a generator on the graph with this index.");
         let effect = self.graph.add_node(NodeData::new2(effect_node));
         let mixer = self
             .graph
@@ -84,17 +112,22 @@ impl RenderGraph {
         self.graph.add_edge(effect, mixer, ());
         self.graph.add_edge(dry, mixer, ());
 
-        self.output_node_index = mixer;
+        // Conect to output summer
+        self.graph.add_edge(mixer, self.output_node_index, ());
+        if let Some(edge) = self.graph.find_edge(dry, self.output_node_index) {
+            self.graph.remove_edge(edge);
+        };
     }
 
     /// Creates a graph that plays the buffer contained in a Vec.
     /// Chain with .add_amp_node to control volume and/or clip.
     pub fn from_vec(vec: Vec<f32>) -> Self {
-        let mut graph = make_graph();
         let sample_count = vec.len();
         let buffer_node: BufferNode = vec.into();
-        let buffer_node_index = graph.add_node(NodeData::new2(BoxedNodeSend::new(buffer_node)));
-        RenderGraph::new(graph, sample_count, buffer_node_index)
+        let mut graph = RenderGraph::default();
+        graph.sample_count = sample_count;
+        graph.add_node(buffer_node);
+        graph
     }
 
     pub fn reset(&mut self) {
