115b2ca6086d5b0f21cba34924b605c9208738ba
Chloe Brett <chloebrett@users.noreply.github.com>
2025-04-18 21:26:50 +1000

    Rename mod_delay_control -> mod_delay_view

--- hydric/src/components/effect/mod.rs
+++ hydric/src/components/effect/mod.rs
@@ -3,11 +3,11 @@ mod delay_view;
 mod effect_window;
 mod eq_view;
 mod mixer_window;
-mod mod_delay_control;
+mod mod_delay_view;
 
 use compressor_view::*;
 use delay_view::*;
 pub use effect_window::*;
 use eq_view::*;
 pub use mixer_window::*;
-use mod_delay_control::*;
+use mod_delay_view::*;
