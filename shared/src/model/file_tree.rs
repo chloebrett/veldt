b6724b8d3ed1b9dba0d1cf01b4633b9eae524b66
Chloe Brett <chloebrett@users.noreply.github.com>
2025-04-19 19:18:14 +1000

    Add a test for the filename tree.

--- shared/src/model/file_tree.rs
+++ shared/src/model/file_tree.rs
@@ -5,6 +5,7 @@ use crate::serialize::map_vec;
 
 pub type FilenameTreeNode = FileTreeNode<String, String>;
 
+#[derive(Debug, PartialEq, Clone)]
 pub enum FileTreeNode</* FileData= */ T, /* DirectoryData= */ U> {
     File(T),
     Directory(U, Vec<FileTreeNode<T, U>>),
@@ -34,5 +35,47 @@ impl From<FilenameTreeNode> for FilenameTreeProto {
                 }
             }),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::testing::proto::proto_testing::assert_proto_round_trip;
+
+    use super::*;
+
+    #[test]
+    fn file_tree_proto_round_trip() {
+        let tree = FilenameTreeNode::Directory(
+            "Samples".to_string(),
+            vec![
+                FilenameTreeNode::Directory(
+                    "Drum kit".to_string(),
+                    vec![
+                        FilenameTreeNode::File("Kick".to_string()),
+                        FilenameTreeNode::Directory(
+                            "Snares".to_string(),
+                            vec![
+                                FilenameTreeNode::File("Snare 1".to_string()),
+                                FilenameTreeNode::File("Snare 2".to_string()),
+                            ],
+                        ),
+                        FilenameTreeNode::File("Hi hat".to_string()),
+                        FilenameTreeNode::File("Tom".to_string()),
+                        FilenameTreeNode::File("Cymbal".to_string()),
+                    ],
+                ),
+                FilenameTreeNode::Directory(
+                    "Loops".to_string(),
+                    vec![
+                        FilenameTreeNode::File("Bass".to_string()),
+                        FilenameTreeNode::File("Guitar".to_string()),
+                        FilenameTreeNode::File("Piano".to_string()),
+                    ],
+                ),
+            ],
+        );
+
+        assert_proto_round_trip::<FilenameTreeNode, FilenameTreeProto>(tree);
     }
 }
