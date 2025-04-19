6d09dea73fbb7e9be043218a82f4549d0c081201
elise <135685125+elisedemarie@users.noreply.github.com>
2025-04-19 15:55:35 +1000

    Fix comment.

--- hydric/src/widget/sequencer.rs
+++ hydric/src/widget/sequencer.rs
@@ -144,7 +144,7 @@ impl<T: SequencerObject<T>, F: Fn(usize, Action), G: Fn(), H: Fn(&mut Ui, usize)
                 .transform(to_sequencer.inverse())
                 .clamp(
                     pos2(0.0, 0.0),
-                    // Clip to `y` range - 1 so that object cannot go off bottom of sequencer.
+                    // Clamp to `y` range - 1 so that object cannot be dragged bottom of sequencer.
                     vec2(f32::INFINITY, self.range.size().y - 1.0).to_pos2(),
                 );
             if drag_delta.y != 0.0 {
