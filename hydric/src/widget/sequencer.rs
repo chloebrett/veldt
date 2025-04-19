09ee6abd59524201ae45122a40c6ff62231bfc5c
elise <135685125+elisedemarie@users.noreply.github.com>
2025-04-19 15:52:34 +1000

    Clamp y of object to stay on sequencer.

--- hydric/src/widget/sequencer.rs
+++ hydric/src/widget/sequencer.rs
@@ -144,7 +144,8 @@ impl<T: SequencerObject<T>, F: Fn(usize, Action), G: Fn(), H: Fn(&mut Ui, usize)
                 .transform(to_sequencer.inverse())
                 .clamp(
                     pos2(0.0, 0.0),
-                    vec2(f32::INFINITY, self.range.size().y).to_pos2(),
+                    // Clip to `y` range - 1 so that object cannot go off bottom of sequencer.
+                    vec2(f32::INFINITY, self.range.size().y - 1.0).to_pos2(),
                 );
             if drag_delta.y != 0.0 {
                 if let Some(action) = object.y_action(scaled_pos.y, self.range) {
