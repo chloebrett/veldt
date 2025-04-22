c5e1712579cc5d419c36896d8527a63ba9d8266e
bowendong1234 <bdon0009@student.monash.edu>
2025-04-22 13:58:03 +1000

    assigned colours to human readable variable names, removed repeated code

--- hydric/src/components/generator/subsynth_control.rs
+++ hydric/src/components/generator/subsynth_control.rs
@@ -15,46 +15,38 @@ where
     F: Fn(Action),
     G: Fn(),
 {
-    // OSC 1
-    ui.push_id(0, |ui| {
-        subsynth_oscillator(
-            &config.oscillators[0],
-            ui,
-            &dispatch,
-            &on_release,
-            Color32::from_rgb(119, 167, 43),
-            Color32::from_rgba_unmultiplied(147, 175, 100, (0.44 * 255.0) as u8),
-        );
-    });
-    ui.add_space(4.0);
+    // opacity initialisation
+    const CHART_FILL_ALPHA: u8 = opacity_percentage_to_alpha(44.0);
 
-    // OSC 2
-    ui.push_id(1, |ui| {
-        subsynth_oscillator(
-            &config.oscillators[1],
-            ui,
-            &dispatch,
-            &on_release,
-            Color32::from_rgb(246, 83, 192),
-            Color32::from_rgba_unmultiplied(212, 132, 170, (0.44 * 255.0) as u8),
-        );
-    });
-    ui.add_space(4.0);
+    const fn opacity_percentage_to_alpha(opacity_percentage: f32) -> u8 {
+        ((opacity_percentage/100.0) * 255.0) as u8
+    }
 
-    // OSC 3
-    ui.push_id(2, |ui| {
-        subsynth_oscillator(
-            &config.oscillators[2],
-            ui,
-            &dispatch,
-            &on_release,
-            Color32::from_rgb(227, 172, 84),
-            Color32::from_rgba_unmultiplied(215, 171, 53, (0.44 * 255.0) as u8),
-        );
-    });
+    // colour related 'constants' - from_rgba_unmultiplied is not a const function so for consistency these are all defined with 'let' and lower snake case
+    let green_outline = Color32::from_rgb(119, 167, 43);
+    let green_fill = Color32::from_rgba_unmultiplied(147, 175, 100, CHART_FILL_ALPHA);
+    let pink_outline = Color32::from_rgb(246, 83, 192);
+    let pink_fill = Color32::from_rgba_unmultiplied(212, 132, 170, CHART_FILL_ALPHA);
+    let orange_outline = Color32::from_rgb(227, 172, 84);
+    let orange_fill = Color32::from_rgba_unmultiplied(215, 171, 53, CHART_FILL_ALPHA);
 
-    ui.add_space(4.0);
-    // piano stuff
+    let line_colours = vec![green_outline, pink_outline, orange_outline];
+    let fill_colours = vec![green_fill, pink_fill, orange_fill];
+
+    for oscillator_id in vec![0, 1, 2] {
+        ui.push_id(oscillator_id, |ui| {
+            subsynth_oscillator(
+                &config.oscillators[oscillator_id],
+                ui,
+                &dispatch,
+                &on_release,
+                line_colours[oscillator_id],
+                fill_colours[oscillator_id],
+            );
+        });
+        ui.add_space(4.0);    
+    }
+
     let min_note: PitchValue = PitchName {
         scale_value: ScaleValue::A,
         octave: 1,
