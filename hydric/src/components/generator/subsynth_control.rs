c732732657a4edd3404ec145ca6557df31d26d03
bowendong1234 <bdon0009@student.monash.edu>
2025-04-23 09:39:04 +1000

    used lazy static for colour related consts and moved draw piano logic into function

--- hydric/src/components/generator/subsynth_control.rs
+++ hydric/src/components/generator/subsynth_control.rs
@@ -3,6 +3,7 @@ use super::subsynth_oscillator;
 use crate::view::View;
 use eframe::egui;
 use egui::{Color32, Ui};
+use lazy_static::lazy_static;
 use shared::model::SubSynthConfig;
 use shared::{
     model::{PitchName, ScaleValue},
@@ -22,42 +23,50 @@ where
         ((opacity_percentage / 100.0) * 255.0) as u8
     }
 
-    // colour related 'constants' - from_rgba_unmultiplied is not a const function so for consistency these are all defined with 'let' and lower snake case
-    let green_outline = Color32::from_rgb(119, 167, 43);
-    let green_fill = Color32::from_rgba_unmultiplied(147, 175, 100, CHART_FILL_ALPHA);
-    let pink_outline = Color32::from_rgb(246, 83, 192);
-    let pink_fill = Color32::from_rgba_unmultiplied(212, 132, 170, CHART_FILL_ALPHA);
-    let orange_outline = Color32::from_rgb(227, 172, 84);
-    let orange_fill = Color32::from_rgba_unmultiplied(215, 171, 53, CHART_FILL_ALPHA);
+    lazy_static! {
+        pub static ref GREEN_OUTLINE: Color32 = Color32::from_rgb(119, 167, 43);
+        pub static ref GREEN_FILL: Color32 =
+            Color32::from_rgba_unmultiplied(147, 175, 100, CHART_FILL_ALPHA);
+        pub static ref PINK_OUTLINE: Color32 = Color32::from_rgb(246, 83, 192);
+        pub static ref PINK_FILL: Color32 =
+            Color32::from_rgba_unmultiplied(212, 132, 170, CHART_FILL_ALPHA);
+        pub static ref ORANGE_OUTLINE: Color32 = Color32::from_rgb(227, 172, 84);
+        pub static ref ORANGE_FILL: Color32 =
+            Color32::from_rgba_unmultiplied(215, 171, 53, CHART_FILL_ALPHA);
+        pub static ref LINE_COLOURS: [Color32; 3] =
+            [*GREEN_OUTLINE, *PINK_OUTLINE, *ORANGE_OUTLINE,];
+        pub static ref FILL_COLOURS: [Color32; 3] = [*GREEN_FILL, *PINK_FILL, *ORANGE_FILL,];
+    }
 
-    let line_colours = vec![green_outline, pink_outline, orange_outline];
-    let fill_colours = vec![green_fill, pink_fill, orange_fill];
-
-    for oscillator_id in vec![0, 1, 2] {
+    for oscillator_id in 0..3 {
         ui.push_id(oscillator_id, |ui| {
             subsynth_oscillator(
                 &config.oscillators[oscillator_id],
                 ui,
                 &dispatch,
                 &on_release,
-                line_colours[oscillator_id],
-                fill_colours[oscillator_id],
+                LINE_COLOURS[oscillator_id],
+                FILL_COLOURS[oscillator_id],
             );
         });
         ui.add_space(4.0);
     }
 
-    let min_note: PitchValue = PitchName {
-        scale_value: ScaleValue::A,
-        octave: 1,
+    fn draw_piano(ui: &mut Ui) {
+        let min_note: PitchValue = PitchName {
+            scale_value: ScaleValue::A,
+            octave: 1,
+        }
+        .into();
+        let max_note: PitchValue = PitchName {
+            scale_value: ScaleValue::C,
+            octave: 8,
+        }
+        .into();
+        Piano::new(max_note, min_note - 1)
+            .with_orientation(PianoOrientation::Horizontal)
+            .ui(ui);
     }
-    .into();
-    let max_note: PitchValue = PitchName {
-        scale_value: ScaleValue::C,
-        octave: 8,
-    }
-    .into();
-    Piano::new(max_note, min_note - 1)
-        .with_orientation(PianoOrientation::Horizontal)
-        .ui(ui);
+
+    draw_piano(ui);
 }
