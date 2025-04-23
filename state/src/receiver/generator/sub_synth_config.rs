5368a4f6ed951c3f2cc16d70b592bc6ce300a277
bredng <bredng01@gmail.com>
2025-04-24 01:00:09 +1000

    Clean up

--- state/src/receiver/generator/sub_synth_config.rs
+++ state/src/receiver/generator/sub_synth_config.rs
@@ -4,7 +4,6 @@ use shared::model::SubSynthConfig;
 
 impl ActionReceiver for SubSynthConfig {
     fn apply(&mut self, action: &Action) -> Option<Action> {
-        // Try applying the action to each oscillator.
         for osc in &mut self.oscillators {
             if let Some(result) = osc.apply(action) {
                 return Some(result);
