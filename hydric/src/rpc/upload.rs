b260250a87e671fa009f40bb4c79334e178fb5f7
maddyyy0110 <tpra0008@student.monash.edu>
2025-04-24 17:15:02 +1000

    Fixed the upload proto and introducted Bytes dep

--- hydric/src/rpc/upload.rs
+++ hydric/src/rpc/upload.rs
@@ -1,14 +1,16 @@
 use shared::consts::XERIC_URL;
 use shared::upload::UploadSampleRequest;
+use shared::upload::upload_client::UploadClient;
 use tonic_web_wasm_client::Client;
+use bytes::Bytes;
 
-pub async fn upload_sample(file_name: String,bytes: web::bytes) -> Result<(), ()>{
+pub async fn upload_sample(file_name: String,bytes: Bytes) -> Result<(), ()>{
     let client = Client::new(XERIC_URL.to_string());
-    let mut grpc = SaveLoadClient::new(client);
+    let mut grpc = UploadClient::new(client);
 
     let result = grpc.upload_sample(UploadSampleRequest {
         name: file_name,
-        data: bytes,
+        uploaded_sample: bytes.to_vec(),
     })
     .await;
 
