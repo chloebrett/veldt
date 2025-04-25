936d930483379b87d02078804ef03c3c70a7c98a
elise <135685125+elisedemarie@users.noreply.github.com>
2025-04-25 19:20:59 +1000

    Remove logger.

--- hydric/src/rpc/save_load.rs
+++ hydric/src/rpc/save_load.rs
@@ -24,7 +24,6 @@ pub async fn load_project_list() -> Result<Vec<String>, ()> {
     let mut grpc = SaveLoadClient::new(client);
 
     let result = grpc.load_project_list(LoadProjectListRequest {}).await;
-    log::info!("{:?}", result);
 
     result
         .map(|it| it.into_inner().project_names)
