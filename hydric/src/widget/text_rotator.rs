be212c26bfa0575ff685c75509a60abe7880d5a7
bowendong1234 <bdon0009@student.monash.edu>
2025-04-26 15:07:11 +1000

    added TextRotation enum and refactored

--- hydric/src/widget/text_rotator.rs
+++ hydric/src/widget/text_rotator.rs
@@ -1,14 +1,19 @@
 use egui::{Color32, FontId, Pos2, Shape, Stroke, Ui, epaint};
 use std::f32::consts::PI;
 
-pub fn rotated_text_bottom_to_top(ui: &mut Ui, text: &str, font_size: f32) {
+pub enum TextRotation {
+    Clockwise,
+    Anticlockwise,
+}
+
+pub fn rotate_text(ui: &mut Ui, text: &str, font_size: f32, rotation: TextRotation) {
     let font_id = FontId::proportional(font_size);
 
     // Galley is essentially the layout/container of the text
     let galley =
         ui.fonts(|fonts| fonts.layout_no_wrap(text.to_string(), font_id.clone(), Color32::WHITE));
     let galley_size = galley.size();
-    let rotated_size = egui::Vec2::new(galley_size.y, galley_size.x);
+    let rotated_size = galley_size.yx();
     let padded_size = rotated_size + egui::Vec2::splat(4.0); // small padding to prevent clipping
 
     // Allocating space in the layout
@@ -17,17 +22,28 @@ pub fn rotated_text_bottom_to_top(ui: &mut Ui, text: &str, font_size: f32) {
 
     // Determining where to draw the rotated text
     let center = rect.center();
-    let draw_pos = Pos2::new(
-        center.x - galley_size.y * 0.5,
-        center.y + galley_size.x * 0.5,
-    );
+    let draw_pos = match rotation {
+        TextRotation::Anticlockwise => Pos2::new(
+            center.x - galley_size.y * 0.5,
+            center.y + galley_size.x * 0.5,
+        ),
+        TextRotation::Clockwise => Pos2::new(
+            center.x + galley_size.y * 0.5,
+            center.y - galley_size.x * 0.5,
+        ),
+    };
+
+    let text_angle = match rotation {
+        TextRotation::Anticlockwise => PI * -0.5,
+        TextRotation::Clockwise => PI * 0.5,
+    };
 
     let text_shape = epaint::TextShape {
         pos: draw_pos,
         galley,
         underline: Stroke::NONE,
         override_text_color: Some(Color32::WHITE),
-        angle: PI * -0.5, // negative PI/2 to make it rotate anti clockwise
+        angle: text_angle,
         fallback_color: Color32::WHITE,
         opacity_factor: 1.0,
     };
