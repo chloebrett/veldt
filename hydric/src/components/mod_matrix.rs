db7c34a6a746ebd962780b974d0f28065516a1d2
bowendong1234 <bdon0009@student.monash.edu>
2025-04-26 19:37:29 +1000

    added row and col titles

--- hydric/src/components/mod_matrix.rs
+++ hydric/src/components/mod_matrix.rs
@@ -1,12 +1,15 @@
-use crate::widget::{get_set, int_slider, knob};
+use crate::widget::{knob, text_rotator, TextRotation};
 use eframe::egui;
 use egui::{Color32, Ui};
 use shared::model::ModMatrix;
-use state::{Action, FloatField, TypeField, UintField};
+use state::{Action, FloatField};
+use wasm_bindgen_futures::js_sys::Array;
 
 pub fn mod_matrix<F, G>(
     config: &ModMatrix,
     ui: &mut egui::Ui,
+    row_titles: Vec<&str>,
+    col_title: Vec<&str>,
     dispatch: &F,
     on_release: &G,
 ) -> egui::Response
@@ -22,18 +25,27 @@ where
 
         frame
             .show(ui, |ui| {
+                ui.horizontal(|ui| {
+                    for col in 0..config.cols {
+                        ui.label(col_title[col as usize]);
+                        if col != config.cols - 1 {
+                            ui.add_space(10.0);
+                        }
+                    }
+                });
                 ui.vertical(|ui| {
                     for row in 0..config.rows {
                         ui.horizontal(|ui| {
+                            text_rotator(ui, row_titles[row as usize], 14.0, TextRotation::Anticlockwise);
                             for col in 0..config.cols {
                                 let id = format!("{:?}", (row, col));
                                 ui.push_id(id, |ui| {
                                     knob(
                                         ui,
                                         "Placeholder",
-                                        0.0, // placeholder 
+                                        0.0,
                                         |it| dispatch(Action::SetFloat(FloatField::Volume, it)),
-                                        0.0..=1.0,
+                                        -1.0..=1.0,
                                         0.0,
                                         &on_release,
                                     );
