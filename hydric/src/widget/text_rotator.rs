a31a5f77bacd0e429090fa472881804fa4c2264b
bowendong1234 <bdon0009@student.monash.edu>
2025-04-27 10:56:54 +1000

    bug fix for neutral text display

--- hydric/src/widget/text_rotator.rs
+++ hydric/src/widget/text_rotator.rs
@@ -14,8 +14,22 @@ pub fn text_rotator(ui: &mut Ui, text: &str, font_size: f32, rotation: TextRotat
     let galley =
         ui.fonts(|fonts| fonts.layout_no_wrap(text.to_string(), font_id.clone(), Color32::WHITE));
     let galley_size = galley.size();
-    let rotated_size = galley_size.yx();
-    let padded_size = rotated_size + egui::Vec2::splat(4.0); // small padding to prevent clipping
+    
+
+    let padded_size = match rotation {
+        TextRotation::Anticlockwise => {
+            let rotated_size = galley_size.yx();
+            rotated_size + egui::Vec2::splat(4.0)
+        },
+        TextRotation::Clockwise => {
+            let rotated_size = galley_size.yx();
+            rotated_size + egui::Vec2::splat(4.0)
+        }
+        ,
+        TextRotation::Neutral => {
+            galley_size + egui::Vec2::splat(4.0)
+        }
+    };
 
     // Allocating space in the layout
     let (rect, _response) = ui.allocate_at_least(padded_size, egui::Sense::hover());
@@ -32,7 +46,7 @@ pub fn text_rotator(ui: &mut Ui, text: &str, font_size: f32, rotation: TextRotat
             center.x + galley_size.y * 0.5,
             center.y - galley_size.x * 0.5,
         ),
-        TextRotation::Neutral => rect.left_bottom()
+        TextRotation::Neutral => rect.left_top()
     };
 
     let text_angle = match rotation {
