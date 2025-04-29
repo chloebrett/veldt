97a428c05a6e69c9f83848ba56957f8ae11e3607
bowendong1234 <bdon0009@student.monash.edu>
2025-04-29 22:36:05 +1000

    use mut_mem to store active tab

--- hydric/src/widget/tab_display.rs
+++ hydric/src/widget/tab_display.rs
@@ -1,8 +1,8 @@
 use egui::{Color32, FontId, Pos2, Shape, Stroke, Ui, epaint, Frame, Vec2};
 use crate::widget::text_rotator;
-use std::hash::Hash;
 use crate::view::View;
 use super::TextRotation;
+use std::fmt;
 
 
 pub enum TabOrientation {
@@ -11,37 +11,65 @@ pub enum TabOrientation {
     Top, // tabs appear on top
 }
 
+// The active tab is stored in mut memory and since mut memory needs a unique id for each item that it stores we define an enum to prevent any double ups from happening
+pub enum TabIdentifier {
+    SubsynthLfo,
+    SubsynthEnv,
+}
+
+impl fmt::Display for TabIdentifier {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TabIdentifier::SubsynthLfo => write!(f, "subsynth lfo tabs"),
+            TabIdentifier::SubsynthEnv => write!(f, "subsynth env tabs"),
+        }
+    }
+}
+
 pub struct TabDisplay<'a>{
+    id: TabIdentifier,
     tab_headings: Vec<&'a str>,
-    tab_contents: Vec<Box<dyn Fn(&mut Ui) + 'a>>,  // Vector of content rendering functions
+    tab_contents: Vec<Box<dyn View>>, // Vector of content rendering functions
     orientation: TabOrientation,
-    active_tab: u8,
 }
 
 impl<'a> TabDisplay<'a> {
     pub fn new(
+        id: TabIdentifier,
         tab_headings: Vec<&'a str>,
-        tab_contents: Vec<Box<dyn Fn(&mut Ui) + 'a>>, 
+        tab_contents: Vec<Box<dyn View>>, 
         orientation: TabOrientation
     ) -> Self {
         TabDisplay {
+            id,
             tab_headings,
             tab_contents,
             orientation,
-            active_tab: 0,
         }
     }
 }
 
 impl View for TabDisplay<'_> {
     fn ui(&mut self, ui: &mut Ui) {
+        let id = &self.id;
         let tab_headings = &self.tab_headings;
         let tab_contents = &self.tab_contents;
         let orientation = &self.orientation;
-        let active_tab = self.active_tab;
-
         let num_tabs = tab_headings.len();
         assert_eq!(num_tabs, tab_contents.len(), "Number of tab headings must match number of tab contents");
+
+        let memory_id = ui.make_persistent_id(id.to_string());
+        
+        // Retrieve the active tab from memory, or default to 0
+        let mut active_tab = ui.memory_mut(|mem: &mut egui::Memory| *mem.data.get_temp_mut_or_default::<usize>(memory_id));
+        
+        // Ensure the active tab is valid
+        if active_tab >= num_tabs && num_tabs > 0 {
+            let new_active_tab = 0 as usize;
+            ui.memory_mut(|mem| mem.data.insert_temp(memory_id, new_active_tab));
+            active_tab = new_active_tab;
+        }
+
 
         const TAB_FONT_SIZE: f32 = 14.0;
 
@@ -91,7 +119,7 @@ impl View for TabDisplay<'_> {
                             ui,
                             heading,
                             TAB_FONT_SIZE,
-                            TextRotation::Anticlockwise,
+                            TextRotation::Anticlockwise90,
                             Color32::BLACK,
                         );
                     }
