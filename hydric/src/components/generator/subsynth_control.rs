4883e6723126c8f8e71ba75f9f57176bfd589ae5
bowendong1234 <bdon0009@student.monash.edu>
2025-04-29 21:08:01 +1000

    refactored as view

--- hydric/src/components/generator/subsynth_control.rs
+++ hydric/src/components/generator/subsynth_control.rs
@@ -1,5 +1,5 @@
 use super::super::{ModMatrixView, Piano, PianoOrientation};
-use super::subsynth_oscillator;
+use super::subsynth_oscillator::SubSynthOscillatorView;
 use crate::view::View;
 use eframe::egui;
 use egui::{Color32, Ui};
@@ -11,77 +11,95 @@ use shared::{
 };
 use state::Action;
 
-pub fn subsynth_control<F, G>(config: &SubSynthConfig, dispatch: F, on_release: G, ui: &mut Ui)
-where
-    F: Fn(Action),
-    G: Fn(),
-{
-    // opacity initialisation
-    const CHART_FILL_ALPHA: u8 = opacity_percentage_to_alpha(44.0);
+pub struct SubSynthControlView<'a, F: Fn(Action), G: Fn()> {
+    config: &'a SubSynthConfig,
+    dispatch: F,
+    on_release: G,
+}
 
-    const fn opacity_percentage_to_alpha(opacity_percentage: f32) -> u8 {
-        ((opacity_percentage / 100.0) * 255.0) as u8
+impl<'a, F: Fn(Action), G: Fn()> SubSynthControlView<'a, F, G> {
+    pub fn new(config: &'a SubSynthConfig, dispatch: F, on_release: G) -> Self {
+        SubSynthControlView {
+            config,
+            dispatch,
+            on_release,
+        }
     }
+}
 
-    lazy_static! {
-        pub static ref GREEN_OUTLINE: Color32 = Color32::from_rgb(119, 167, 43);
-        pub static ref GREEN_FILL: Color32 =
-            Color32::from_rgba_unmultiplied(147, 175, 100, CHART_FILL_ALPHA);
-        pub static ref PINK_OUTLINE: Color32 = Color32::from_rgb(246, 83, 192);
-        pub static ref PINK_FILL: Color32 =
-            Color32::from_rgba_unmultiplied(212, 132, 170, CHART_FILL_ALPHA);
-        pub static ref ORANGE_OUTLINE: Color32 = Color32::from_rgb(227, 172, 84);
-        pub static ref ORANGE_FILL: Color32 =
-            Color32::from_rgba_unmultiplied(215, 171, 53, CHART_FILL_ALPHA);
-        pub static ref LINE_COLOURS: [Color32; 3] =
-            [*GREEN_OUTLINE, *PINK_OUTLINE, *ORANGE_OUTLINE,];
-        pub static ref FILL_COLOURS: [Color32; 3] = [*GREEN_FILL, *PINK_FILL, *ORANGE_FILL,];
-    }
+impl<F: Fn(Action), G: Fn()> View for SubSynthControlView<'_, F, G> {
+    fn ui(&mut self, ui: &mut Ui) {
+        let config = self.config;
+        let dispatch = &self.dispatch;
+        let on_release = &self.on_release;
 
-    ui.horizontal(|ui| {
-        ui.vertical(|ui| {
-            for oscillator_id in 0..3 {
-                ui.push_id(oscillator_id, |ui| {
-                    subsynth_oscillator(
-                        &config.oscillators[oscillator_id],
-                        ui,
-                        &dispatch,
-                        &on_release,
-                        LINE_COLOURS[oscillator_id],
-                        FILL_COLOURS[oscillator_id],
-                    );
-                    ui.add_space(4.0);
-                });
+        // opacity initialisation
+        const CHART_FILL_ALPHA: u8 = opacity_percentage_to_alpha(44.0);
+
+        const fn opacity_percentage_to_alpha(opacity_percentage: f32) -> u8 {
+            ((opacity_percentage / 100.0) * 255.0) as u8
+        }
+
+        lazy_static! {
+            pub static ref GREEN_OUTLINE: Color32 = Color32::from_rgb(119, 167, 43);
+            pub static ref GREEN_FILL: Color32 =
+                Color32::from_rgba_unmultiplied(147, 175, 100, CHART_FILL_ALPHA);
+            pub static ref PINK_OUTLINE: Color32 = Color32::from_rgb(246, 83, 192);
+            pub static ref PINK_FILL: Color32 =
+                Color32::from_rgba_unmultiplied(212, 132, 170, CHART_FILL_ALPHA);
+            pub static ref ORANGE_OUTLINE: Color32 = Color32::from_rgb(227, 172, 84);
+            pub static ref ORANGE_FILL: Color32 =
+                Color32::from_rgba_unmultiplied(215, 171, 53, CHART_FILL_ALPHA);
+            pub static ref LINE_COLOURS: [Color32; 3] =
+                [*GREEN_OUTLINE, *PINK_OUTLINE, *ORANGE_OUTLINE,];
+            pub static ref FILL_COLOURS: [Color32; 3] = [*GREEN_FILL, *PINK_FILL, *ORANGE_FILL,];
+        }
+
+        ui.horizontal(|ui| {
+            ui.vertical(|ui| {
+                for oscillator_id in 0..3 {
+                    ui.push_id(oscillator_id, |ui| {
+                        SubSynthOscillatorView::new(
+                            &config.oscillators[oscillator_id],
+                            &dispatch,
+                            &on_release,
+                            LINE_COLOURS[oscillator_id],
+                            FILL_COLOURS[oscillator_id],
+                        )
+                        .ui(ui);
+                        ui.add_space(4.0);
+                    });
+                }
+            });
+            ui.add_space(5.0);
+            ui.vertical(|ui| {
+                ModMatrixView::new(
+                    &config.matrix_config,
+                    vec!["ENV 1", "ENV 2", "ENV 3", "LFO 1", "LFO 2", "LFO 3"],
+                    vec!["OSC 1", "OSC 2", "OSC 3"],
+                    dispatch,
+                    on_release,
+                )
+                .ui(ui); // must wrap in ui.vertical to stop the matrix from unnecessarily stretching vertically
+            });
+        });
+
+        fn draw_piano(ui: &mut Ui) {
+            let min_note: PitchValue = PitchName {
+                scale_value: ScaleValue::A,
+                octave: 1,
             }
-        });
-        ui.add_space(5.0);
-        ui.vertical(|ui| {
-            ModMatrixView::new(
-                &config.matrix_config,
-                vec!["ENV 1", "ENV 2", "ENV 3", "LFO 1", "LFO 2", "LFO 3"],
-                vec!["OSC 1", "OSC 2", "OSC 3"],
-                dispatch,
-                on_release,
-            )
-            .ui(ui); // must wrap in ui.vertical to stop the matrix from unnecessarily stretching vertically
-        });
-    });
+            .into();
+            let max_note: PitchValue = PitchName {
+                scale_value: ScaleValue::C,
+                octave: 8,
+            }
+            .into();
+            Piano::new(max_note, min_note - 1)
+                .with_orientation(PianoOrientation::Horizontal)
+                .ui(ui);
+        }
 
-    fn draw_piano(ui: &mut Ui) {
-        let min_note: PitchValue = PitchName {
-            scale_value: ScaleValue::A,
-            octave: 1,
-        }
-        .into();
-        let max_note: PitchValue = PitchName {
-            scale_value: ScaleValue::C,
-            octave: 8,
-        }
-        .into();
-        Piano::new(max_note, min_note - 1)
-            .with_orientation(PianoOrientation::Horizontal)
-            .ui(ui);
+        draw_piano(ui);
     }
-
-    draw_piano(ui);
 }
