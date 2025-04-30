2b62e1550438cc393b1c90eeee9e105e00bbeb25
maddyyy0110 <tpra0008@student.monash.edu>
2025-04-30 16:57:18 +1000

    Refactored front end to use chunk stream.

--- hydric/src/rpc/upload.rs
+++ hydric/src/rpc/upload.rs
@@ -1,18 +1,42 @@
 use shared::consts::XERIC_URL;
-use shared::upload::UploadSampleRequest;
+use shared::upload::UploadChunkRequest;
 use shared::upload::upload_client::UploadClient;
 use tonic_web_wasm_client::Client;
 
-pub async fn upload_sample(file_name: String, bytes: Vec<u8>) -> Result<(), ()> {
+pub async fn upload_sample(file_name: String, bytes: Vec<u8>) -> Result<(), String> {
+    // Streams chunks of data now.
+
     let client = Client::new(XERIC_URL.to_string());
     let mut grpc = UploadClient::new(client);
 
-    let result = grpc
-        .upload_sample(UploadSampleRequest {
-            name: file_name,
-            uploaded_sample: bytes,
-        })
-        .await;
+    /*
+    Chunk size in MB, I believe the error point is > 4MB.
+    Increasing this beyond 4 will create a network error, likely as result of tonic or grpc.
+    */
+    const CHUNK_SIZE: usize = 3 * 1024 * 1024;
 
-    result.map(|_| ()).map_err(|_| ())
+    let total_chunks = (bytes.len() + CHUNK_SIZE - 1) / CHUNK_SIZE;
+    let total_chunks = total_chunks as u32; // Borrow checker gets angry if we leave as uSize.
+
+    // ::iter borrows from the inputted bytes, so we need to take ownsership.
+    let owned_chunks: Vec<Vec<u8>> = bytes
+        .chunks(CHUNK_SIZE)
+        .map(|chunk| chunk.to_vec())
+        .collect();
+
+    // Create stream.
+    let stream =
+        futures::stream::iter(owned_chunks.into_iter().enumerate().map(move |(i, chunk)| {
+            UploadChunkRequest {
+                file_name: file_name.clone(),
+                chunk_data: chunk.to_vec(),
+                chunk_index: i as u32,
+                total_chunks,
+            }
+        }));
+
+    grpc.upload_sample(stream)
+        .await
+        .map(|_| ()) //Currently discarding response.
+        .map_err(|e| format!("Upload failed: {:?}", e))
 }
