a53be2c27a1f642d2d818c93e501b4c4aa256369
bowendong1234 <bdon0009@student.monash.edu>
2025-04-30 13:36:58 +1000

    added states for active tabs for subsynth

--- hydric/src/app_state.rs
+++ hydric/src/app_state.rs
@@ -119,6 +119,8 @@ pub enum DataState {
     DragCursorDelta,
     TrackRollSelectMode,
     NoteRollSelectMode,
+    SubSynthLfoTab,
+    SubSynthEnvTab,
 }
 
 impl DataState {
@@ -135,6 +137,8 @@ impl DataState {
             Self::DragCursorDelta => "drag_start_from",
             Self::TrackRollSelectMode => "track_roll_select_mode",
             Self::NoteRollSelectMode => "note_roll_select_mode",
+            Self::SubSynthLfoTab => "subsynth_lfo_tab_index",
+            Self::SubSynthEnvTab => "subsynth_env_tab_index",
         })
     }
 
@@ -163,7 +167,9 @@ impl DataState {
                 | Self::NoteRollSelectMode => data.insert_temp::<Option<bool>>(self.get_id(), None),
                 Self::ActiveNoteIndex
                 | Self::ActiveTrackIndex
-                | Self::ActiveTrackPlacementIndex => {
+                | Self::ActiveTrackPlacementIndex 
+                | Self::SubSynthLfoTab
+                | Self::SubSynthEnvTab => {
                     data.insert_temp::<Option<usize>>(self.get_id(), None)
                 }
                 Self::SelectedNoteIndexes | Self::SelectedTrackPlacementIndexes => {
