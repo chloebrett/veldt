ceec6e0e87229a2e60ab6fea111e22211cc96096
elise <135685125+elisedemarie@users.noreply.github.com>
2025-05-03 09:36:20 +1000

    Improve comment.

--- hydric/src/components/effect/mixer_view.rs
+++ hydric/src/components/effect/mixer_view.rs
@@ -128,7 +128,7 @@ impl View for MixerView<'_> {
 }
 
 /// A widget to display and edit basic effect controls in the MixerView
-/// Makes it easier to drag and drop.
+/// Being a widget that returns a `Reponse` makes it easier to drag and drop.
 struct EffectWidget<'a, F: Fn(Action), G: Fn()> {
     effect: &'a EffectInstance,
     effect_window: &'a mut WindowStateField<EffectSelector>,
