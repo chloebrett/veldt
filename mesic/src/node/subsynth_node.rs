1a1015c6ff008310415dc8bb68aa72695931cde7
Chloe Brett <chloebrett@users.noreply.github.com>
2025-05-08 15:45:59 +1000

    Create SubSynthWaveSource.

--- mesic/src/node/subsynth_node.rs
+++ mesic/src/node/subsynth_node.rs
@@ -1,10 +1,12 @@
 use super::pan_multipliers;
+use crate::SAMPLE_RATE;
 use crate::consts::CHANNEL_COUNT;
 use crate::graph::ProcessContext;
-use crate::wave::{WaveSource, multi_sum};
+use crate::wave::{WaveCache, WaveKey, WaveSource, detune_multiplier, linspace, multi_sum};
+use dasp_frame::Stereo;
 use dasp_graph::{Buffer, Input, Node};
-use shared::model::{Generator, GeneratorInstance, GeneratorMeta, SubSynthConfig};
-use shared::types::{Beats, KnobPosition, Volume};
+use shared::model::{Generator, Oscillator, GeneratorInstance, GeneratorMeta, PitchName, SubSynthConfig, AntiAliasingMode};
+use shared::types::{Beats, Freq, KnobPosition, Volume};
 use state::GeneratorSelector;
 
 pub struct SubSynthNode {
@@ -131,5 +133,64 @@ impl Node<ProcessContext> for SubSynthNode {
             let meta = &self.state.meta;
             Self::apply_volume_and_pan(out_buf, channel_index, meta.volume, meta.pan);
         }
+    }
+}
+
+pub struct SubSynthWaveSource {
+    // TODO: recycle the wave cache?
+    // Currently it's re-created each time the note changes.
+    cache: WaveCache,
+    pitch: PitchName,
+    oscillator: Oscillator,
+    sample_index: usize,
+}
+
+impl SubSynthWaveSource {
+    pub fn new(pitch: PitchName, oscillator: Oscillator) -> Self {
+        Self {
+            cache: WaveCache::default(),
+            pitch,
+            oscillator,
+            sample_index: 0,
+        }
+    }
+
+    pub fn same_pitch(&self, pitch: PitchName) -> bool {
+        self.pitch == pitch
+    }
+}
+
+impl Iterator for SubSynthWaveSource {
+    type Item = Stereo<f32>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut output_mono = 0.0;
+        let freq: Freq = self.pitch.into();
+        let osc = &self.oscillator;
+        let freq = freq * detune_multiplier(osc.osc_detune);
+
+        let detunes = linspace(-osc.unison_detune, osc.unison_detune, osc.osc_count);
+
+        for detune in detunes {
+            let freq = freq * detune_multiplier(detune);
+            let step = freq / (SAMPLE_RATE as f32);
+            let phase = ((self.sample_index as f32) * step) % 1.0;
+
+            let key = WaveKey {
+                kind: osc.wave,
+                aa: AntiAliasingMode::Off,
+                freq: freq.into(),
+            };
+            output_mono += self.cache.get(&key, phase);
+        }
+
+        let mut output_stereo = [output_mono, output_mono];
+        for channel_index in 0..CHANNEL_COUNT {
+            let pan_mult = pan_multipliers(osc.pan)[channel_index];
+            output_stereo[channel_index] *= pan_mult * osc.volume;
+        }
+
+        self.sample_index += 1;
+        Some(output_stereo)
     }
 }
