1ec53f402a1998305c99b96f39ddd76603c836d5
Chloe Brett <chloebrett@users.noreply.github.com>
2025-05-08 16:16:50 +1000

    Remove unused track() function and rename track2 to track.

--- mesic/src/graph/note_tracker.rs
+++ mesic/src/graph/note_tracker.rs
@@ -42,76 +42,8 @@ pub enum NoteEventType {
 pub struct NoteTracker;
 
 impl NoteTracker {
-    pub fn track(project: &Project, global_sample_index: usize) -> NoteEventsByGenerator {
-        let mut result: NoteEventsByGenerator = vec![vec![]; project.generators.len()];
-
-        let bpm = project.bpm;
-
-        // TODO: make this loop more efficient, instead of looping over generators one by one.
-        for generator_index in 0..project.generators.len() {
-            let placements: Vec<_> = project
-                .placements
-                .clone()
-                .into_iter()
-                .filter(|it| match &it.kind {
-                    PlacementType::Track(it) => it.generator_index == generator_index,
-                    _ => false,
-                })
-                .collect();
-
-            for placement in &placements {
-                let &Ok(&TrackPlacement { track_index, .. }) = &placement.try_into() else {
-                    continue;
-                };
-                let track = &project.tracks[track_index];
-                let track_offset = *placement.offset;
-                let track_duration = *placement
-                    .clipped_duration
-                    .unwrap_or(track.unclipped_duration());
-                let track_end_sample = beats_to_samples(track_offset + track_duration, bpm);
-
-                // TODO: use some kind of tree to determine which notes are in range of the current
-                // buffer, instead of always iterating over all notes.
-                // Then apply the same idea to tracks.
-                for note in &track.notes {
-                    let offset = beats_to_samples(track_offset + *note.offset, bpm);
-                    let note_start_sample = min(offset, track_end_sample) as usize;
-                    let note_end_sample = min(
-                        offset + beats_to_samples(note.note.beats, bpm),
-                        track_end_sample,
-                    ) as usize;
-
-                    // Don't play notes that aren't relevant to this buffer segment.
-                    if note_start_sample > global_sample_index + Buffer::LEN
-                        || note_end_sample < global_sample_index
-                    {
-                        continue;
-                    }
-
-                    let start_sample = (note_start_sample as i32 - global_sample_index as i32)
-                        .clamp(0, Buffer::LEN as i32)
-                        as usize;
-                    let end_sample = (note_end_sample as i32 - global_sample_index as i32)
-                        .clamp(0, Buffer::LEN as i32) as usize;
-
-                    let event = NoteEvent {
-                        pitch_name: note.note.pitch_name,
-                        samples_since_started: global_sample_index as i32
-                            - note_start_sample as i32,
-                        duration: note.note.beats,
-                        start_sample,
-                        end_sample,
-                    };
-
-                    result[generator_index].push(event);
-                }
-            }
-        }
-        result
-    }
-
     /// TODO: make all generator nodes use note on/off events, so we can get rid of 'NoteTracker::track' and take the '2' out of this method name.
-    pub fn track2(project: &Project, global_sample_index: usize) -> Vec<Vec<NoteEvent2>> {
+    pub fn track(project: &Project, global_sample_index: usize) -> Vec<Vec<NoteEvent2>> {
         let mut result: Vec<Vec<NoteEvent2>> = vec![vec![]; project.generators.len()];
 
         let bpm = project.bpm;
