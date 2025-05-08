d01a0b4e50987440316d1ec615353a10f085e3e0
Chloe Brett <chloebrett@users.noreply.github.com>
2025-05-08 16:23:34 +1000

    Remove unused NoteEvent types.

--- mesic/src/graph/note_tracker.rs
+++ mesic/src/graph/note_tracker.rs
@@ -1,32 +1,13 @@
 use crate::wave::beats_to_samples;
 use dasp_graph::Buffer;
 use shared::model::{PitchName, PlacementType, Project, TrackPlacement};
-use shared::types::Beats;
 use std::cmp::min;
 
 // Outer index is generator index.
 pub type NoteEventsByGenerator = Vec<Vec<NoteEvent>>;
 
-#[derive(Clone)]
-pub struct NoteEvent {
-    pub pitch_name: PitchName,
-
-    // Global values.
-    // Can be negative.
-    pub samples_since_started: i32,
-    pub duration: Beats,
-
-    // Local values.
-    // Within the current buffer. Always 0 - 64.
-    pub start_sample: usize,
-    // Within the current buffer. Always 0 - 64.
-    pub end_sample: usize,
-}
-
-/// TODO: make all generator nodes use note on/off events, so we can get rid of the original
-/// `NoteEvent` and rename this one to it.
 #[derive(Clone, Debug)]
-pub struct NoteEvent2 {
+pub struct NoteEvent {
     pub kind: NoteEventType,
     pub sample_index: usize,
     pub pitch_name: PitchName,
@@ -42,9 +23,8 @@ pub enum NoteEventType {
 pub struct NoteTracker;
 
 impl NoteTracker {
-    /// TODO: make all generator nodes use note on/off events, so we can get rid of 'NoteTracker::track' and take the '2' out of this method name.
-    pub fn track(project: &Project, global_sample_index: usize) -> Vec<Vec<NoteEvent2>> {
-        let mut result: Vec<Vec<NoteEvent2>> = vec![vec![]; project.generators.len()];
+    pub fn track(project: &Project, global_sample_index: usize) -> NoteEventsByGenerator {
+        let mut result: NoteEventsByGenerator = vec![vec![]; project.generators.len()];
 
         let bpm = project.bpm;
 
@@ -87,7 +67,7 @@ impl NoteTracker {
                     let start_sample = note_start_sample as isize - global_sample_index as isize;
                     if buf_range.contains(&start_sample) {
                         result.push({
-                            NoteEvent2 {
+                            NoteEvent {
                                 kind: NoteEventType::On,
                                 sample_index: start_sample as usize,
                                 pitch_name: note.note.pitch_name,
@@ -98,7 +78,7 @@ impl NoteTracker {
                     let end_sample = note_end_sample as isize - global_sample_index as isize;
                     if buf_range.contains(&end_sample) {
                         result.push({
-                            NoteEvent2 {
+                            NoteEvent {
                                 kind: NoteEventType::Off,
                                 sample_index: end_sample as usize,
                                 pitch_name: note.note.pitch_name,
