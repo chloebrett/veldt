ee881f31fb97f9329ded6b614c2ec7ce06a71bb2
bdan0005 <bdan0005@student.monash.edu>
2025-05-08 22:06:05 +1000

    Fixes

--- hydric/src/components/generator/subsynth_lpf.rs
+++ hydric/src/components/generator/subsynth_lpf.rs
@@ -32,7 +32,7 @@ impl<F: Fn(Action), G: Fn()> View for SubSynthLpfView<'_, F, G> {
         const TEXT_COLOUR: Color32 = Color32::from_gray(180);
 
         let frame = egui::Frame::new()
-            .fill(Color32::from_rgb(50, 50, 50))
+            .fill(Color32::from_gray(50))
             .stroke(egui::Stroke::new(1.0, Color32::from_gray(60)))
             .corner_radius(8.0)
             .inner_margin(6.0);
@@ -59,16 +59,6 @@ impl<F: Fn(Action), G: Fn()> View for SubSynthLpfView<'_, F, G> {
                 |it| dispatch(Action::SetFloat(FloatField::Q, it)),
                 0.1..=100.0,
                 1.0,
-                || on_release(),
-            );
-
-            knob(
-                ui,
-                "Gain",
-                config.gain,
-                |it| dispatch(Action::SetFloat(FloatField::Gain, it)),
-                -60.0..=60.0,
-                0.0,
                 || on_release(),
             );
         });
