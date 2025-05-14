163784942aa67d47dbbd3a93d9df6137d5e381b5
bdan0005 <bdan0005@student.monash.edu>
2025-05-15 05:41:55 +1000

    Fix comment

--- hydric/src/components/generator/noise.rs
+++ hydric/src/components/generator/noise.rs
@@ -7,7 +7,7 @@ use state::{Action, GeneratorSelector, TypeField};
 use strum::IntoEnumIterator;
 
 pub struct NoiseView<'a, F: Fn(Action), G: Fn()> {
-    selector: GeneratorSelector, // NOTE: could be used in the future for configuration/multiple noise generators
+    selector: GeneratorSelector, 
     config: &'a NoiseConfig,
     player: &'a mut AudioPlayer,
     dispatch: F,
