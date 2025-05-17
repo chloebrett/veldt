732c04a446be411afc624f0164264b7cf8c16c36
elise <135685125+elisedemarie@users.noreply.github.com>
2025-05-17 12:36:33 +1000

    Create method to create channel shapes.

--- hydric/src/components/audio_level.rs
+++ hydric/src/components/audio_level.rs
@@ -2,7 +2,7 @@ use egui::{
     Color32, CornerRadius, Frame, Rect, Response, Sense, Shape, Ui, Vec2, Widget,
     emath::RectTransform, pos2, vec2,
 };
-use egui::{InnerResponse, Rangef};
+use egui::{InnerResponse, Pos2, Rangef};
 
 use crate::playback::AudioPlayer;
 use crate::transform::Transform;
@@ -25,7 +25,7 @@ impl<'a> AudioLevel<'a> {
         }
     }
 
-    fn create_level_shapes(&self, range: Rect, left_level: f32, right_level: f32) -> Shape {
+    fn create_channel_shape(&self, range: Rect, level: f32) -> Shape {
         // Padding around level line.
         let side_padding = 0.2;
         let top_padding = 5.0;
@@ -35,59 +35,52 @@ impl<'a> AudioLevel<'a> {
         // Marker appears at peak of level.
         // Marker will be twice this height.
         let marker_height = 0.5;
-        let left_channel = |level, min_value| {
+        let channel_rect = |top, bottom| {
             Rect::from_x_y_ranges(
                 Rangef {
-                    min: side_padding,
-                    max: (range.size().x - side_padding) * 0.5,
-                },
-                Rangef {
-                    min: level * padded_y_size - top_padding,
-                    max: min_value * padded_y_size - top_padding,
-                },
-            )
-        };
-        let right_channel = |level, min_value| {
-            Rect::from_x_y_ranges(
-                Rangef {
-                    min: (range.size().x + side_padding) * 0.5,
+                    min: range.left() + side_padding,
                     max: range.right() - side_padding,
                 },
                 Rangef {
-                    min: level * padded_y_size - top_padding,
-                    max: min_value * padded_y_size - top_padding,
+                    min: top * padded_y_size - top_padding,
+                    max: bottom * padded_y_size - top_padding,
                 },
             )
         };
+        Shape::Vec(vec![
+            // Background of channel.
+            Shape::rect_filled(
+                channel_rect(range.top(), range.bottom()),
+                CornerRadius::same(0),
+                Color32::from_black_alpha(64),
+            ),
+            // Level of channel.
+            Shape::rect_filled(
+                channel_rect(level, range.bottom()),
+                CornerRadius::same(0),
+                Color32::WHITE,
+            ),
+            // Peak  marker
+            Shape::rect_filled(
+                channel_rect(level - marker_height, level + marker_height),
+                CornerRadius::same(0),
+                Color32::BLACK,
+            ),
+        ])
+    }
 
-        // Shape behind level for each channel.
-        let left_background_rect = left_channel(range.top(), range.bottom());
-        let right_background_rect = right_channel(range.top(), range.bottom());
-        // Level of each channel.
-        let left_level_rect = left_channel(left_level, range.bottom());
-        let right_level_rect = right_channel(right_level, range.bottom());
-        // Marker at peak of level.
-        let left_marker = left_channel(left_level + marker_height, left_level - marker_height);
-        let right_marker = right_channel(right_level + marker_height, right_level - marker_height);
-
-        let shapes: Vec<_> = [
-            [left_background_rect, left_level_rect, left_marker],
-            [right_background_rect, right_level_rect, right_marker],
-        ]
-        .into_iter()
-        .flat_map(|[background, level, marker]| {
-            vec![
-                Shape::rect_filled(
-                    background,
-                    CornerRadius::same(0),
-                    Color32::from_black_alpha(64),
-                ),
-                Shape::rect_filled(level, CornerRadius::same(0), Color32::WHITE),
-                Shape::rect_filled(marker, CornerRadius::same(0), Color32::BLACK),
-            ]
-        })
-        .collect();
-        Shape::Vec(shapes)
+    fn create_level_shape(&self, range: Rect, left_level: f32, right_level: f32) -> Shape {
+        // Divide range for left and right channels.
+        let split_range_size = range.size() * vec2(0.5, 1.0);
+        let left_range = Rect::from_min_size(range.left_top(), split_range_size);
+        let right_range = Rect::from_min_size(
+            range.left_top() + vec2(split_range_size.x, 0.0),
+            split_range_size,
+        );
+        Shape::Vec(vec![
+            self.create_channel_shape(left_range, left_level),
+            self.create_channel_shape(right_range, right_level),
+        ])
     }
 }
 
@@ -104,7 +97,7 @@ impl Widget for AudioLevel<'_> {
         let [left_level, right_level] = player.level();
         let InnerResponse { inner: _, response } = Frame::canvas(ui.style()).show(ui, |ui| {
             let (response, painter) = ui.allocate_painter(size, Sense::all());
-            let level_shapes = self.create_level_shapes(range, left_level, right_level);
+            let level_shapes = self.create_level_shape(range, left_level, right_level);
             let to_screen = RectTransform::from_to(range, response.rect);
             // Add background shape.
             painter.add(Shape::rect_filled(
