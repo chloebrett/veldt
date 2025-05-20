6249d037d1d95c6fc30bcfcfa3bfb98625ee317a
Chloe Brett <chloebrett@users.noreply.github.com>
2025-05-20 19:44:41 +1000

    2024 edition -> 2021 edition.

--- hydric/src/promise.rs
+++ hydric/src/promise.rs
@@ -1,4 +1,5 @@
 use poll_promise::Promise;
+use std::future::Future;
 
 // Async states represent outgoing requests that may or may not have finished.
 // The Option<Promise<Result<..., ()>>> format is for the following reasons:
