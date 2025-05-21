a5620f670479d115780665d1f1cbf80a16a4fc47
Chloe Brett <chloebrett@users.noreply.github.com>
2025-05-21 20:21:59 +1000

    Move 'recreate mixer' to a debug options dropdown.

--- hydric/src/components/play/play_control.rs
+++ hydric/src/components/play/play_control.rs
@@ -38,10 +38,6 @@ pub fn play_control(
     ui.separator();
 
     ui.horizontal(|ui| {
-        // TODO: create a debug options dropdown in the main menu and put this there.
-        if ui.button("Recreate mixer (for debug)").clicked() {
-            player.refresh_mixer();
-        }
         if ui.button("Set audio from server").clicked() {
             let project = store.get().project.clone();
             spawn(&mut async_state.server_render, async move {
