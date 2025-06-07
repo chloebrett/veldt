c9f72b26bd36ca4e6b20157b1386aed01341e767
Chloe Brett <chloebrett@users.noreply.github.com>
2025-06-07 21:26:44 +1000

    Sample hash map - changes to shared.

---
+++ shared/src/model/id.rs
@@ -0,0 +1,70 @@
+use std::ops::Deref;
+
+#[derive(Hash, Default, PartialEq, Eq, Ord, PartialOrd, Debug, Clone, Copy)]
+pub struct GeneratorId(pub usize);
+
+impl From<u32> for GeneratorId {
+    fn from(other: u32) -> Self {
+        Self(other as usize)
+    }
+}
+
+impl From<GeneratorId> for u32 {
+    fn from(other: GeneratorId) -> Self {
+        *other as u32
+    }
+}
+
+impl Deref for GeneratorId {
+    type Target = usize;
+
+    fn deref(&self) -> &usize {
+        &self.0
+    }
+}
+
+#[derive(Hash, Default, PartialEq, Eq, Ord, PartialOrd, Debug, Clone, Copy)]
+pub struct PlacementId(pub usize);
+
+impl From<u32> for PlacementId {
+    fn from(other: u32) -> Self {
+        Self(other as usize)
+    }
+}
+
+impl From<PlacementId> for u32 {
+    fn from(other: PlacementId) -> Self {
+        *other as u32
+    }
+}
+
+impl Deref for PlacementId {
+    type Target = usize;
+
+    fn deref(&self) -> &usize {
+        &self.0
+    }
+}
+
+#[derive(Hash, Default, PartialEq, Eq, Ord, PartialOrd, Debug, Clone, Copy)]
+pub struct SampleId(pub usize);
+
+impl From<u32> for SampleId {
+    fn from(other: u32) -> Self {
+        Self(other as usize)
+    }
+}
+
+impl From<SampleId> for u32 {
+    fn from(other: SampleId) -> Self {
+        *other as u32
+    }
+}
+
+impl Deref for SampleId {
+    type Target = usize;
+
+    fn deref(&self) -> &usize {
+        &self.0
+    }
+}
