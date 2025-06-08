f7197f58cf96c74c5242c79ab5e997873134ef37
bdan0005 <bdan0005@student.monash.edu>
2025-06-09 03:42:33 +1000

    Fix multistage pink filter cutoffs

--- mesic/src/node/noise_generator_node.rs
+++ mesic/src/node/noise_generator_node.rs
@@ -1,3 +1,4 @@
+use crate::consts::SAMPLE_RATE;
 use crate::eq::eq_filter;
 use crate::eq::filter::Filter;
 use crate::graph::{NoteEvent, NoteEventType, ProcessContext};
@@ -7,7 +8,6 @@ use shared::model::{
     EqConfig, EqType, Generator, GeneratorInstance, GeneratorMeta, NoiseConfig, NoiseType,
 };
 use state::GeneratorSelector;
-use crate::consts::SAMPLE_RATE;
 
 pub struct NoiseGeneratorNode {
     selector: GeneratorSelector,
@@ -34,8 +34,8 @@ impl Default for NodeState {
             pink_filters: Self::initialise_pink_filters(),
             brown_filter: eq_filter(&EqConfig {
                 kind: EqType::SimpleFirstOrderLowPass,
-                fc: 20.0, 
-                q: 0.707, 
+                fc: 20.0,
+                q: 0.707,  // See "Designing Audio Effect Plugins in C++", W. Pirkle, p273
                 gain: 0.0,
             }),
         }
@@ -59,27 +59,40 @@ impl NodeState {
         }
     }
 
-    // New helper function to initialize your pink noise filter chain
     fn initialise_pink_filters() -> Vec<Filter> {
         let mut filters = Vec::new();
 
         filters.push(eq_filter(&EqConfig {
             kind: EqType::SimpleFirstOrderLowPass,
             fc: SAMPLE_RATE as f32 / 4.0,
+            q: 0.707, 
+            gain: 0.0,
+        }));
+
+        filters.push(eq_filter(&EqConfig {
+            kind: EqType::SimpleFirstOrderLowPass,
+            fc: SAMPLE_RATE as f32 / 8.0, 
             q: 0.707,
             gain: 0.0,
         }));
 
         filters.push(eq_filter(&EqConfig {
             kind: EqType::SimpleFirstOrderLowPass,
-            fc: SAMPLE_RATE as f32 / 16.0,
+            fc: SAMPLE_RATE as f32 / 16.0, 
             q: 0.707,
             gain: 0.0,
         }));
 
         filters.push(eq_filter(&EqConfig {
             kind: EqType::SimpleFirstOrderLowPass,
-            fc: SAMPLE_RATE as f32 / 32.0,
+            fc: SAMPLE_RATE as f32 / 32.0, 
+            q: 0.707,
+            gain: 0.0,
+        }));
+
+        filters.push(eq_filter(&EqConfig {
+            kind: EqType::SimpleFirstOrderLowPass,
+            fc: SAMPLE_RATE as f32 / 64.0,
             q: 0.707,
             gain: 0.0,
         }));
@@ -177,86 +190,4 @@ impl Node<ProcessContext> for NoiseGeneratorNode {
 // Generates random number between [min, max]
 pub fn generate_random_number_in_range(rng: &mut impl Rng, min: f32, max: f32) -> f32 {
     rng.gen_range(min..=max)
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::node::NoiseGeneratorNode;
-    use dasp_graph::Buffer;
-    use shared::model::GeneratorId;
-    use state::GeneratorSelector;
-
-    const FLOAT_THRES: f32 = 1e-3;
-
-    struct PkCoefficients {
-        b0: f32,
-        b1: f32,
-        b2: f32,
-        b3: f32,
-        b4: f32,
-        b5: f32,
-        b6: f32,
-    }
-
-    impl PkCoefficients {
-        fn pk_pink_noise(&mut self, white: f32) -> f32 {
-            self.b0 = 0.99886 * self.b0 + white * 0.0555179;
-            self.b1 = 0.99332 * self.b1 + white * 0.0750759;
-            self.b2 = 0.96900 * self.b2 + white * 0.1538520;
-            self.b3 = 0.86650 * self.b3 + white * 0.3104856;
-            self.b4 = 0.55000 * self.b4 + white * 0.5329522;
-            self.b5 = -0.7616 * self.b5 - white * 0.0168980;
-            let pink = self.b0
-                + self.b1
-                + self.b2
-                + self.b3
-                + self.b4
-                + self.b5
-                + self.b6
-                + white * 0.5362;
-            self.b6 = white * 0.115926;
-
-            pink
-        }
-    }
-
-    fn assert_almost_equal(first: Buffer, second: Buffer) {
-        if first.len() != second.len() {
-            panic!("Lengths differed! {}, {}", first.len(), second.len());
-        }
-        for i in 0..first.len() {
-            let a = first[i];
-            let b = second[i];
-            if (a - b).abs() > FLOAT_THRES {
-                panic!("Floats {a}, {b} differed at index {i}");
-            }
-        }
-    }
-
-    #[ignore]
-    #[test]
-    fn test_pink_noise() {
-        let mut pk_coeff = PkCoefficients {
-            b0: 0.0,
-            b1: 0.0,
-            b2: 0.0,
-            b3: 0.0,
-            b4: 0.0,
-            b5: 0.0,
-            b6: 0.0,
-        };
-        let mut node = NoiseGeneratorNode::new(GeneratorSelector(GeneratorId(0)));
-        let mut rng = rand::thread_rng();
-        let mut buffer = Buffer::SILENT;
-        let mut pk_buffer = Buffer::SILENT;
-
-        for i in 0..buffer.len() {
-            let white = node.generate_white_noise(&mut rng);
-            pk_buffer[i] = pk_coeff.pk_pink_noise(white);
-        }
-
-        node.generate_pink_noise(&mut buffer);
-
-        assert_almost_equal(buffer, pk_buffer);
-    }
 }
