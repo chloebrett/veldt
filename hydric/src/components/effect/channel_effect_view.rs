13134c199fb873c02a24c2621129cc8ac7b73bf5
elise <135685125+elisedemarie@users.noreply.github.com>
2025-06-09 15:23:49 +1000

    Remove TODO.

--- hydric/src/components/effect/channel_effect_view.rs
+++ hydric/src/components/effect/channel_effect_view.rs
@@ -34,7 +34,6 @@ impl View for ChannelEffectView<'_> {
         let Self {
             store, local_state, ..
         } = *self;
-        // TODO: Should only the active Mixer channel be shown?
         let selector = local_state
             .active_mixer_channel
             .get()
