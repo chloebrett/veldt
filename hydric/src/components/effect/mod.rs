edc2757e1c0dc6d6143f74fccc44a593f2323f24
elise <135685125+elisedemarie@users.noreply.github.com>
2025-06-09 12:42:55 +1000

    Create ChannelEffectView.

--- hydric/src/components/effect/mod.rs
+++ hydric/src/components/effect/mod.rs
@@ -5,6 +5,7 @@ mod eq_view;
 mod mixer_matrix;
 mod mixer_view;
 mod mod_delay_view;
+mod channel_effect_view;
 
 use compressor_view::*;
 use delay_view::*;
@@ -13,3 +14,4 @@ use eq_view::*;
 pub use mixer_matrix::*;
 pub use mixer_view::*;
 use mod_delay_view::*;
+use channel_effect_view::*;
