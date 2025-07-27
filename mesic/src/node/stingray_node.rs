102b8a65b93e3de8cc149439aeb762e0753b8931
Victor <rotciv3210@gmail.com>
2025-07-27 19:42:02 +1000

    formatting

--- mesic/src/node/stingray_node.rs
+++ mesic/src/node/stingray_node.rs
@@ -251,9 +251,6 @@ impl Node<ProcessContext> for StingrayNode {
             new_lpf_freq = new_lpf_freq.clamp(LPF_MIN_FREQ, LPF_MAX_FREQ);
             let mut new_eq_config = state.config.lpf.clone();
             new_eq_config.fc = new_lpf_freq.into();
-
-            
-
         }
 
         for (channel_index, out_buf) in output.iter_mut().enumerate() {
