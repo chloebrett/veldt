f8245bf6dcaf014c644b3b7afed5a84e643baec0
bowendong1234 <bdon0009@student.monash.edu>
2025-08-24 10:30:20 +1000

    placement header and sample preview boilerplate

--- hydric/src/components/track_roll.rs
+++ hydric/src/components/track_roll.rs
@@ -1,7 +1,7 @@
 use crate::{GetSet, LocalState, transform::Transform};
 use crate::{view::View, widget::StateWindow, window_state::WindowKind};
-use egui::PointerButton;
-use egui::epaint::RectShape;
+use egui::{Galley, PointerButton};
+use egui::epaint::{RectShape, TextShape};
 use egui::{
     Color32, CornerRadius, CursorIcon, Frame, Pos2, Rect, Response, ScrollArea, Sense, Shape,
     Stroke, StrokeKind, Ui, Vec2, Widget, emath::RectTransform, pos2, vec2,
@@ -10,7 +10,7 @@ use mesic::samples_to_beats;
 use ordered_float::OrderedFloat;
 use shared::{
     model::{
-        PlacedNote, Placement, PlacementId, PlacementType, SamplePlacement, Track, TrackPlacement,
+        PlacedNote, Placement, PlacementId, PlacementType, SamplePlacement, Track, TrackPlacement, SampleId
     },
     types::Beats,
 };
@@ -146,7 +146,8 @@ impl View for TrackRoll<'_> {
                                 .horizontal_rects(
                                     |index| index % 2 == 1,
                                     Color32::from_white_alpha(1),
-                                ),
+                            )
+
                         );
                     });
             },
@@ -228,14 +229,21 @@ impl<'a> PlacedTrack<'a> {
                         )
                     }
                 }
-                PlacementType::Sample(_) => Shape::rect_filled(
-                    self.to_rect(range),
-                    CornerRadius::same(1),
-                    background_colour,
-                ),
+                PlacementType::Sample(sample_placement) => {
+                    let sample_id = sample_placement.sample_id;
+                    self.sample_shape(range, sample_id)
+                }
+,
             },
-            self.create_header_shape(range),
+            self.create_header_shape(range), // header label is created separately via create_label as textshape galley requires access to Ui
         ])
+    }
+
+    fn sample_shape(&self, range: Rect, sample_id: SampleId) -> Shape{
+        if let Some(sample) = self.store.get().project.samples.get(&sample_id) {
+            
+        }
+        Shape::Noop
     }
 
     fn map_notes_to_shapes(&self, range: Rect, notes: &Vec<PlacedNote>) -> Shape {
@@ -260,9 +268,9 @@ impl<'a> PlacedTrack<'a> {
                     note_rect,
                     1.5,
                     Color32::from_rgb_additive(
-                        rgb_values[0] as u8,
-                        rgb_values[1] as u8,
-                        rgb_values[2] as u8,
+                        rgb_values[0],
+                        rgb_values[1],
+                        rgb_values[2],
                     ),
                     Stroke::NONE,
                     StrokeKind::Inside,
@@ -284,9 +292,9 @@ impl<'a> PlacedTrack<'a> {
     fn create_header_shape(&self, range: Rect) -> Shape {
         let rgb_values = self.placement.colour;
         let header_brackground_colour = Color32::from_rgb_additive(
-            rgb_values[0] as u8,
-            rgb_values[1] as u8,
-            rgb_values[2] as u8,
+            rgb_values[0],
+            rgb_values[1],
+            rgb_values[2],
         );
 
         let mut header_rect = Rect::from_pos(Pos2::ZERO);
@@ -302,6 +310,26 @@ impl<'a> PlacedTrack<'a> {
             .transform(placement_transform);
 
         header_shape
+    }
+
+    fn create_label(&self, range: Rect, ui: &mut Ui) -> Shape {
+        let font_id = egui::FontId::proportional(12.0);
+        let galley = ui.fonts(|fonts| fonts.layout_no_wrap(("Please I beg").to_string(), font_id.clone(), Color32::BLACK));
+        let transform = RectTransform::from_to(
+            Rect::from_min_max(Pos2::ZERO, Pos2::new(*self.unclipped_duration, PITCH_RANGE)),
+            self.to_rect(range),
+        );
+        let position = Pos2::ZERO.transform(transform);
+        let text_shape = Shape::Text(TextShape {
+            pos: position,
+            galley,
+            underline: Stroke::NONE,
+            override_text_color: Some(Color32::BLACK),
+            angle: 0.0,
+            fallback_color: Color32::BLACK,
+            opacity_factor: 1.0,
+        });
+        text_shape
     }
 
     fn get_active(store: &'a Store, local_state: &LocalState) -> Option<PlacedTrack<'a>> {
@@ -333,9 +361,9 @@ impl<'a> PlacedTrack<'a> {
                 Stroke {
                     width: 1.5,
                     color: Color32::from_rgb_additive(
-                        rgb_values[0] as u8,
-                        rgb_values[1] as u8,
-                        rgb_values[2] as u8,
+                        rgb_values[0],
+                        rgb_values[1],
+                        rgb_values[2],
                     ),
                 },
                 StrokeKind::Inside,
@@ -652,6 +680,15 @@ impl<'a> TrackSequencer<'a> {
                 .collect(),
         )
     }
+
+    fn object_labels(&self, ui: &mut Ui) -> Shape {
+        Shape::Vec(
+            self.objects
+                .values()
+                .map(|object| object.create_label(self.range, ui))
+                .collect(),
+        )
+    }
 }
 
 impl Widget for TrackSequencer<'_> {
@@ -707,6 +744,7 @@ impl Widget for TrackSequencer<'_> {
 
             painter.extend(self.background_shapes.clone().transform(to_screen));
             painter.add(self.object_shapes().transform(to_screen));
+            painter.add(self.object_labels(ui).transform(to_screen));
 
             if let Some(object) = PlacedTrack::get_active(self.store, self.local_state) {
                 painter.add(object.active_shape(range).transform(to_screen));
