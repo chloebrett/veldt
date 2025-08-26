133e6016ce5373732d2e994a12d5baf2eeeafa0b
Bowen <126026189+bowendong1234@users.noreply.github.com>
2025-08-26 16:22:01 +1000

    More track/placement improvements (#581)
    * add placement header shape
    
    * colour utils
    
    * bug fix
    
    * load sample into project correctly
    
    * use functions from colour utils
    
    * placement header and sample preview boilerplate
    
    * render sample wave implementation
    
    * cache points for performance
    
    * fix label
    
    * fix get_active for samples
    
    * dynamically adjust placement font colour based on luminance of placement colour for better legibility
    
    * snip notes instead of shrinking if placement is clipped
    
    * make sample visualisation clip instead of shrink
    
    * formatting
    
    * more clean up

--- hydric/src/components/play/sample_tree_view.rs
+++ hydric/src/components/play/sample_tree_view.rs
@@ -8,7 +8,7 @@ use crate::{AsyncState, playback::AudioPlayer};
 use egui::{Checkbox, ScrollArea, Ui};
 use egui_ltreeview::{Action as TreeAction, TreeView, TreeViewBuilder};
 use mesic::interleave_stereo;
-use shared::model::{FileTree, FileTreeConfig, FilenameTree};
+use shared::model::{FileTree, FileTreeConfig, FilenameTree, Sample};
 use state::{Action, Store, TypeField};
 
 pub struct SampleTreeView<'a> {
@@ -207,6 +207,9 @@ impl View for SampleTreeView<'_> {
                                     .sample_cache
                                     .borrow_mut()
                                     .insert(sample.sample_name.clone(), sample.clone());
+                                // load into project
+                                self.store
+                                    .dispatchr(Action::AddChild(TypeField::Sample(sample.clone())));
                             });
                         });
                 }
