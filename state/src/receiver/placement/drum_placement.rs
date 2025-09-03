06f8ba22ad9ba8bf6a0689a237f6da59c9466061
maureen019 <85357818+maureen019@users.noreply.github.com>
2025-09-04 06:11:31 +1000

    update drum placement action

--- state/src/receiver/placement/drum_placement.rs
+++ state/src/receiver/placement/drum_placement.rs
@@ -2,14 +2,13 @@ use crate::receiver::ActionReceiver;
 use crate::{Action, TypeField};
 use shared::model::DrumPlacement;
 
-//TODO: double check grouping of placements
 impl ActionReceiver for DrumPlacement {
     fn apply(&mut self, action: &Action) -> Option<Action> {
         Some(match action {
-            Action::SetChild(TypeField::TrackId(track_id)) => {
-                let prev = self.track_id;
-                self.track_id = *track_id;
-                Action::SetChild(TypeField::TrackId(prev))
+            Action::SetChild(TypeField::DrumTrackId(drum_track_id)) => {
+                let prev = self.drum_track_id;
+                self.drum_track_id = *drum_track_id;
+                Action::SetChild(TypeField::DrumTrackId(prev))
             }
             _ => return None,
         })
