c83638338df4ae33fe450bb51f9ae5e7dc45e58d
bowendong1234 <bdon0009@student.monash.edu>
2025-09-07 12:26:56 +1000

    make project duration take samples into account

--- shared/src/model/project.rs
+++ shared/src/model/project.rs
@@ -1,6 +1,5 @@
 use crate::model::{
-    DrumTrack, DrumTrackId, EffectId, EffectInstance, GeneratorId, GeneratorInstance, Mixer,
-    Placement, PlacementId, Sample, SampleId, Track, TrackId, TrackPlacement,
+    DrumTrack, DrumTrackId, EffectId, EffectInstance, GeneratorId, GeneratorInstance, Mixer, Placement, PlacementId, PlacementType, Sample, SampleId, Track, TrackId, TrackPlacement, samples_to_beats
 };
 use crate::pmodel::*;
 use crate::types::Beats;
@@ -40,14 +39,27 @@ impl Project {
     pub fn duration(&self) -> OrderedFloat<f32> {
         let mut max = OrderedFloat(0.0);
         for placement in self.placements.values() {
-            if let &Ok(&TrackPlacement { track_id, .. }) = &placement.try_into() {
-                let track = &self.tracks[&track_id];
-                let offset = &placement.offset;
-                let duration = placement
-                    .clipped_duration
-                    .unwrap_or(track.unclipped_duration());
-                max = std::cmp::max(max, offset + duration);
-            }
+            let offset = &placement.offset;
+            let duration = match &placement.kind {
+                PlacementType::Track(track_placement) => {
+                    let track = &self.tracks[&track_placement.track_id];
+                    placement
+                        .clipped_duration
+                        .unwrap_or(track.unclipped_duration())
+                }
+                PlacementType::Sample(sample_placement) => {
+                    if let Some(sample) = self.samples.get(&sample_placement.sample_id) {
+                        placement.clipped_duration.unwrap_or(OrderedFloat(samples_to_beats(std::cmp::max(sample.left.len(), sample.right.len()), self.bpm)))
+                    } else {
+                        OrderedFloat(0.0)
+                    }
+                    
+                }
+                PlacementType::DrumTrack(_) => 
+                    //TODO DRUM TRACK TIME
+                    todo!()   
+            };
+            max = std::cmp::max(max, offset + duration);
         }
         max
     }
--- shared/src/model/sample.rs
+++ shared/src/model/sample.rs
@@ -1,5 +1,6 @@
 use crate::bytes::{as_bytes, as_floats};
 use crate::pmodel::*;
+use crate::types::Beats;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Sample {
@@ -29,4 +30,13 @@ impl From<Sample> for SampleProto {
             sample_name: item.sample_name,
         }
     }
+}
+
+// This function is copied from Mesic. Duplicating here instead of importing to avoid Mesic dependency
+pub const SAMPLE_RATE: i32 = 44_100;
+pub const SECONDS_PER_MINUTE: f32 = 60.0;
+
+pub fn samples_to_beats(samples: usize, bpm: Beats) -> Beats {
+    let seconds = samples as f32 / SAMPLE_RATE as f32;
+    seconds * bpm / SECONDS_PER_MINUTE
 }
