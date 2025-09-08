1b7317cd6a430f921b147b074af0fead21f94c86
bowendong1234 <bdon0009@student.monash.edu>
2025-09-08 11:54:18 +1000

    fix sub track id 0

--- state/src/selector/drum_sub_track.rs
+++ state/src/selector/drum_sub_track.rs
@@ -1,30 +1,22 @@
-use super::{PlacementSelector, Selector, SelectorTrait};
+use super::{Selector, SelectorTrait};
 use crate::StoreData;
-use crate::selector::{PlacementId, SampleId};
-use shared::model::{DrumSubTrack, DrumTrack};
-
-impl DrumSubTrackSelector {
-    pub fn upcast(&self) -> PlacementSelector {
-        PlacementSelector(self.0)
-    }
-}
+use crate::selector::{DrumTrackId, SampleId};
+use shared::model::DrumSubTrack;
 
 #[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Debug, Hash)]
-pub struct DrumSubTrackSelector(pub PlacementId, pub SampleId);
+pub struct DrumSubTrackSelector(pub DrumTrackId, pub SampleId);
 
 impl SelectorTrait for DrumSubTrackSelector {
     type Item = DrumSubTrack;
 
     fn try_select<'a>(&'a self, store: &'a StoreData) -> Option<&'a Self::Item> {
-        let placement = store.project.placements.get(&self.0)?;
-        let drum_placement: &DrumTrack = (placement).try_into().ok()?;
-        drum_placement.drum_sub_tracks.get(&self.1)
+        let drum_track = store.project.drum_tracks.get(&self.0).unwrap();
+        drum_track.drum_sub_tracks.get(&self.1)
     }
 
     fn try_select_mut<'a>(&'a self, store: &'a mut StoreData) -> Option<&'a mut Self::Item> {
-        let placement = store.project.placements.get_mut(&self.0)?;
-        let drum_placement: &mut DrumTrack = (placement).try_into().ok()?;
-        drum_placement.drum_sub_tracks.get_mut(&self.1)
+        let drum_track = store.project.drum_tracks.get_mut(&self.0).unwrap();
+        drum_track.drum_sub_tracks.get_mut(&self.1)
     }
 
     fn as_enum(&self) -> Selector {
