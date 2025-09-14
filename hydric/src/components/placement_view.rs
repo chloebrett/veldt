71499f28104bb5cf7e411d0ec5557522cb17600e
maureen019 <85357818+maureen019@users.noreply.github.com>
2025-09-14 23:15:32 +1000

    update drum placement ui

--- hydric/src/components/placement_view.rs
+++ hydric/src/components/placement_view.rs
@@ -210,38 +210,52 @@ impl<'a> PlacementView<'a> {
             ui.label("No drum tracks added yet");
             return;
         };
+        
+        let samples_exist = !store.get().project.samples.is_empty();
+        let selected_text = if samples_exist {
+        Self::get_sample_name(store, &drum_placement.sample_id)
+        } else {
+            "No samples".to_string()
+        };
+        ui.horizontal(|ui| {
+            ui.set_width(185.0);
+                egui::ComboBox::from_id_salt(format!("placement_{:?}", placement_id))
+                    .selected_text(Self::get_drum_track_name(store, &drum_placement.track_id))
+                    .show_ui(ui, |ui| {
+                        for track_id in store.get().project.tracks.keys() {
+                            selectable_value(
+                                ui,
+                                get_set(&drum_placement.track_id, |it| {
+                                    store.dispatch(sel, Action::SetChild(TypeField::TrackId(*it)))
+                                }),
+                                track_id,
+                                Self::get_drum_track_name(store, track_id),
+                            );
+                        }
+                    });
 
-        egui::ComboBox::from_id_salt(format!("placement_{:?}", placement_id))
-            .selected_text(Self::get_drum_track_name(store, &drum_placement.track_id))
-            .show_ui(ui, |ui| {
-                for track_id in store.get().project.tracks.keys() {
-                    selectable_value(
-                        ui,
-                        get_set(&drum_placement.track_id, |it| {
-                            store.dispatch(sel, Action::SetChild(TypeField::TrackId(*it)))
-                        }),
-                        track_id,
-                        Self::get_drum_track_name(store, track_id),
-                    );
-                }
+                egui::ComboBox::from_id_salt(format!("drum_placement_sample{:?}", placement_id))
+                    .selected_text(selected_text)
+                    .show_ui(ui, |ui| {
+                        if samples_exist {
+                            for sample_id in store.get().project.samples.keys() {
+                                selectable_value(
+                                    ui,
+                                    get_set(&drum_placement.sample_id, |it| {
+                                        store.dispatch(sel, Action::SetChild(TypeField::SampleId(*it)))
+                                    }),
+                                    sample_id,
+                                    Self::get_sample_name(store, sample_id),
+                                );
+                            }
+                        } else {
+                            ui.label("No samples to select");
+                        }
+
+                });
             });
 
-        egui::ComboBox::from_id_salt(format!("drum_placement_sample{:?}", placement_id))
-            .selected_text(Self::get_sample_name(store, &drum_placement.sample_id))
-            .show_ui(ui, |ui| {
-                for sample_id in store.get().project.samples.keys() {
-                    selectable_value(
-                        ui,
-                        get_set(&drum_placement.sample_id, |it| {
-                            store.dispatch(sel, Action::SetChild(TypeField::SampleId(*it)))
-                        }),
-                        sample_id,
-                        Self::get_sample_name(store, sample_id),
-                    );
-                }
-            });
-
-        ui.add_space(3.0);
+        ui.add_space(5.0);
     }
 
     fn get_drum_track_name(store: &Store, track_id: &TrackId) -> String {
@@ -272,7 +286,7 @@ impl View for PlacementView<'_> {
             WindowKind::Placement,
             &title,
             |ui| {
-                ui.add_space(3.0);
+                ui.add_space(5.0);
                 match &placement.kind {
                     PlacementType::Track(track_placement) => {
                         Self::track_placement_ui(
